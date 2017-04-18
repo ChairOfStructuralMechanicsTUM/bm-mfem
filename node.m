e7b2afb2c943dc240f1934f9eed93cec76f42417
qaumann <quirin.aumann@tum.de>
2017-04-18 15:16:56 +0200

    Added .gitignore

--- node.m
+++ node.m
@@ -1,11 +1,43 @@
-classdef node
-    %UNTITLED Summary of this class goes here
+classdef node < handle %handle class
+    %NODE The node class
     %   Detailed explanation goes here
     
-    properties
+    properties (Access = private)
+        id
+        x
+        y
+        z
     end
     
     methods
+        % constructor
+        function node = node(id, x, y, z)
+            switch nargin
+                case 3
+                    node.id = id;
+                    node.x = x;
+                    node.y = y;
+                case 4
+                    node.id = id;
+                    node.x = x;
+                    node.y = y;
+                    node.z = z;
+                otherwise
+                    error('Wrong number of arguments')
+            end
+        end
+        
+        % getters
+        function id = getId(node)
+            id = node.id;
+        end
+        
+        function coords = getCoords(node)
+           coords = [node.x node.y node.z]; 
+        end
+        
+        
+        
     end
     
 end
