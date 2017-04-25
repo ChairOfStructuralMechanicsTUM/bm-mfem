17b6d6b0508b957ca74ab7fda0ce2bfd057d1332
cschmausser <cschmausser@users.noreply.github.com>
2017-04-25 17:48:00 +0200

    Added function getNodalForces(femModel)

--- core/solvers/SimpleSolvingStrategy.m
+++ core/solvers/SimpleSolvingStrategy.m
@@ -3,18 +3,54 @@ classdef SimpleSolvingStrategy < Solver
     %   Detailed explanation goes here
     
     properties
+%         nodalDisplacments
     end
     
-    methods (Static)
-        function solve(femModel)
-            K = SimpleAssembler.assembleGlobalStiffnessMatrix(femModel);
-            f = SimpleAssembler.applyExternalForces(femModel);
-            
-            x = K \ f';
-            
-            SimpleAssembler.assignResultsToDofs(femModel, x);
+
+
+    %     methods
+        
+        %     constructor
+%         function nodalDisplacements = SimpleSolvingStrategy(femModel)
+%             if (nargin > 0)
+%                 nodalDisplacements.res = SimpleSolvingStrategy.solve(femModel);
+%             end
+%         end
+%     end
+    
+    
+    
+methods (Static)
+    function x = solve(femModel)
+        Kred = SimpleAssembler(femModel).reducedStiffnessMatrix;
+        f = SimpleAssembler(femModel).reducedForceVector;
+        
+        x = Kred \ f';
+        
+        SimpleAssembler.assignResultsToDofs(femModel, x);
+    end
+    
+ 
+    
+    function nodalForces = getNodalForces(femModel)
+        
+        dofs = femModel.getDofArray;
+        nDofs = length(dofs);
+        nodalForces = SimpleAssembler(femModel).forceVector;
+        fixedDofs = [];
+        
+        for itDof = 1:nDofs
+            if (dofs(itDof).isFixed)
+                fixedDofs = [fixedDofs itDof];                          % array of fixed dofs and their location
+            else
+                nodalForces(itDof) = dofs(itDof).getValue;
+            end
         end
+        
+        nodalForces(fixedDofs) = SimpleAssembler(femModel).stiffnessMatrix(fixedDofs, :) * getValue(femModel.dofArray)';
+        
     end
     
+end
 end
 
