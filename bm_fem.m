4afa80da4a80667e998b6ae0ac8785c0f9f66da2
qaumann <quirin.aumann@tum.de>
2017-04-27 12:53:30 +0200

    New folder for external libraries

--- bm_fem.m
+++ bm_fem.m
@@ -1,8 +1,21 @@
-% initiate the bm-fem tools
+% initialize the bm-fem tools
+disp('     __                    ______________  ___')
+disp('    / /_  ____ ___        / ____/ ____/  |/  /')
+disp('   / __ \/ __ `__ \______/ /_  / __/ / /|_/ / ')
+disp('  / /_/ / / / / / /_____/ __/ / /___/ /  / /  ')
+disp(' /_.___/_/ /_/ /_/     /_/   /_____/_/  /_/   ')
+disp('Initializing bm-fem')
 
+% add all folders to the search path
 addpath('core');
 addpath('core/assemblers');
 addpath('core/solvers');
 addpath('elements');
+if (exist('external_libs','dir') == 7)
+    addpath(genpath('external_libs'));
+end
 addpath('tests');
 addpath('utilities');
+
+% switch off unnecessary warnings
+warning off MATLAB:handle_graphics:exceptions:SceneNode
