563b6b70b502a6381506358023db804c67b330f8
cschmausser <cschmausser@users.noreply.github.com>
2017-05-08 18:00:16 +0200

    Added main-file for example with input file

---
+++ Bridge_with_inputFile.m
@@ -0,0 +1,17 @@
+io = ModelIO('tests/validation_bridge_input.msh');
+            model = io.readModel;
+            
+            model.getModelPart('fixed_support').fixDof('DISPLACEMENT_X');
+            model.getModelPart('fixed_support').fixDof('DISPLACEMENT_Y');
+            model.getModelPart('roller_support').fixDof('DISPLACEMENT_Y');
+            model.getAllNodes.fixDof('DISPLACEMENT_Z');
+            
+            addPointLoad(model.getNodes([3 5 9 11]),10,[0 -1]);
+            addPointLoad(model.getNode(7),16,[0 -1]);
+            
+            SimpleSolvingStrategy.solve(model);
+            
+            actualDisplacementX = model.getAllNodes.getDofValue('DISPLACEMENT_X');
+            actualDisplacementY = model.getAllNodes.getDofValue('DISPLACEMENT_Y');
+            
+            createOutputFile(model);
