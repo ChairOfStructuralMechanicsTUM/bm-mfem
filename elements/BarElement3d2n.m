f3b371b48d10b30243779d9abdcd3f8999a0e303
qaumann <qaumann@users.noreply.github.com>
2017-05-08 09:57:59 +0200

    Reverted test

--- elements/BarElement3d2n.m
+++ elements/BarElement3d2n.m
@@ -3,7 +3,6 @@ classdef BarElement3d2n < BarElement
     %   Detailed explanation goes here
     
     properties (Access = private)
-        length %TEST!
     end
     
     properties (Access = private, Constant = true)
