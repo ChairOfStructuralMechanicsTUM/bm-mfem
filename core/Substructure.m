cb17a631f0c5bbe96868e47f9bdb4a8b2527b3ea
SchaalF <SchaalF@users.noreply.github.com>
2017-06-14 11:38:40 +0200

    Added the property of nodes at the interface. this is needed to find the boolean matrix. here the nodes at the interface are needed.

--- core/Substructure.m
+++ core/Substructure.m
@@ -3,21 +3,30 @@ classdef Substructure < FemModel
     %   This class is a substructure of the mesh in a FemModel. It can be 
     %   used to create substructes which are later used for FETI methods
     
-    properties
+    properties (Access = private)
+        nodesIntf
     end 
     
     methods
         %constructor
-        function substructur = Substructure(nodeArray, elementArray, femModelParts)
+        function substructur = Substructure(nodeArray, elementArray, nodesIntf, femModelParts)
             if nargin == 0
                 super_args = {};
             elseif nargin == 2
                 super_args = {nodeArray, elementArray};
             elseif nargin == 3
+                super_args = {nodeArray, elementArray};
+            elseif nargin == 4
                 super_args = {nodeArray, elementArray, femModelParts};
             end
             substructur@FemModel(super_args{:});
-        end       
+            substructur.nodesIntf = nodesIntf;
+        end    
+        
+        %getter
+        function nodesIntf = getNodesIntf(substructur)
+            nodesIntf = substructur.nodesIntf;
+        end
     end
         
         
