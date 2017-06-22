089c6f8ff6c482179785d8d0bb7163cf4b28ade9
qaumann <qaumann@users.noreply.github.com>
2017-06-22 12:10:17 +0200

    Adding the transformation matrix

--- elements/SpringDamperElement3d2n.m
+++ elements/SpringDamperElement3d2n.m
@@ -18,8 +18,8 @@ classdef SpringDamperElement3d2n < Element
             % call the super class constructor
             springDamperElement@Element(super_args{:});
             springDamperElement.dofNames = cellstr(['DISPLACEMENT_X'; 'DISPLACEMENT_Y'; 'DISPLACEMENT_Z']);
-            springDamperElement.requiredProperties = [ ];
-            springDamperElement.required3dProperties = cellstr(["ELEMENTAL_STIFFNESS", "ELEMENTAL_DAMPING"]);
+            springDamperElement.requiredProperties = cellstr(["ELEMENTAL_STIFFNESS", "ELEMENTAL_DAMPING"]);
+            springDamperElement.required3dProperties = [ ];
             
             % the constructor
             if nargin > 0
@@ -35,7 +35,7 @@ classdef SpringDamperElement3d2n < Element
             
         end
         
-        function stiffnessMatrix = computeLocalStiffnessMatrix(springDamperElement)
+        function stiffnessMatrix = OLDcomputeLocalStiffnessMatrix(springDamperElement)
            stiffness = springDamperElement.getPropertyValue('ELEMENTAL_STIFFNESS');
            stiffnessMatrix = zeros(6);
            for ii = 1:size(stiffnessMatrix,1)/2
@@ -46,36 +46,52 @@ classdef SpringDamperElement3d2n < Element
            end
         end
         
+        function stiffnessMatrix = computeLocalStiffnessMatrix(element)
+           stiffness = element.getPropertyValue('ELEMENTAL_STIFFNESS');
+           stiffnessMatrix = zeros(6);
+           stiffnessMatrix(1,1) = stiffness;
+           stiffnessMatrix(1,4) = - stiffness;
+           stiffnessMatrix(4,4) = stiffness;
+           stiffnessMatrix(4,1) = - stiffness;
+           
+           tMat = element.getTransformationMatrix;
+           stiffnessMatrix = tMat' * stiffnessMatrix * tMat;
+        end
+        
         function massMatrix = computeLocalMassMatrix(element)
             massMatrix = zeros(6);
         end
         
-        function dampingMatrix = computeLocalDampingMatrix(springDamperElement)
-           stiffness = springDamperElement.getPropertyValue('ELEMENTAL_DAMPING');
+        function dampingMatrix = computeLocalDampingMatrix(element)
+           damping = element.getPropertyValue('ELEMENTAL_DAMPING');
            dampingMatrix = zeros(6);
-           for ii = 1:size(dampingMatrix,1)/2
-               dampingMatrix(ii, ii) = stiffness(ii);
-               dampingMatrix(ii+3, ii+3) = stiffness(ii);
-               dampingMatrix(ii, ii+3) = -stiffness(ii);
-               dampingMatrix(ii+3, ii) = -stiffness(ii);
-           end
+           dampingMatrix(1,1) = damping;
+           dampingMatrix(1,4) = - damping;
+           dampingMatrix(4,4) = damping;
+           dampingMatrix(4,1) = - damping;
+           
+           tMat = element.getTransformationMatrix;
+           dampingMatrix = tMat' * dampingMatrix * tMat;           
         end
         
-        function forceVector = computeLocalForceVector(ele)
+        function forceVector = computeLocalForceVector(element)
            forceVector = zeros(1,6);
-           stiffness = ele.getPropertyValue('ELEMENTAL_STIFFNESS');
-           nodes = ele.getNodes;
-           du = nodes(2).getDofValue('DISPLACEMENT_X') - nodes(1).getDofValue('DISPLACEMENT_X');
-           dv = nodes(2).getDofValue('DISPLACEMENT_Y') - nodes(1).getDofValue('DISPLACEMENT_Y');
-           dw = nodes(2).getDofValue('DISPLACEMENT_Z') - nodes(1).getDofValue('DISPLACEMENT_Z');
+           stiffness = element.getPropertyValue('ELEMENTAL_STIFFNESS');
+           nodes = element.getNodes;
            
-           forceVector(1) = + stiffness(1) * du;
-           forceVector(2) = + stiffness(2) * dv;
-           forceVector(3) = + stiffness(3) * dw;
-           forceVector(4) = - stiffness(1) * du;
-           forceVector(5) = - stiffness(2) * dv;
-           forceVector(6) = - stiffness(3) * dw;
-        end
+           disp = zeros(1,3);
+           disp(1) = nodes(2).getDofValue('DISPLACEMENT_X') - nodes(1).getDofValue('DISPLACEMENT_X');
+           disp(2) = nodes(2).getDofValue('DISPLACEMENT_Y') - nodes(1).getDofValue('DISPLACEMENT_Y');
+           disp(3) = nodes(2).getDofValue('DISPLACEMENT_Z') - nodes(1).getDofValue('DISPLACEMENT_Z');
+           disp = norm(disp);
+           
+           forceVector(1) = stiffness * disp;
+           forceVector(4) =  - stiffness * disp;
+           
+           tMat = element.getTransformationMatrix;
+           forceVector = tMat' * forceVector';
+           forceVector = forceVector';
+        end        
         
         function update(springDamperElement)
             springDamperElement.length0 = computeLength(springDamperElement.nodeArray(1).getCoords, ...
@@ -97,6 +113,29 @@ classdef SpringDamperElement3d2n < Element
                 springDamperElement.nodeArray.getY + ...
                 springDamperElement.nodeArray.getDofValue('DISPLACEMENT_Y'));
         end
+    end
+    
+    methods (Access = private)
+       
+        function tMat = getTransformationMatrix(ele)
+            dirX = ele.nodeArray(2).getCoords - ele.nodeArray(1).getCoords;
+            dirX = dirX ./ norm(dirX);
+            
+            dirY = cross([0 0 1], dirX);
+            dirY = dirY ./ norm(dirY);
+            dirZ = cross(dirX, dirY);
+            dirZ = dirZ ./ norm(dirZ);
+            
+            T=zeros(3);         % the scalar product is applied implicitly here
+            T(1,:) = dirX;
+            T(2,:) = dirY;
+            T(3,:) = dirZ;
+           
+            tMat = zeros(6);
+            tMat(1:3,1:3) = T;
+            tMat(4:6,4:6) = T;
+        end
+        
     end
     
 end
