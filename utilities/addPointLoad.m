378ae47a7279217fd94472b2bf88da68a36bfdff
SchaalF <SchaalF@users.noreply.github.com>
2017-07-03 11:51:03 +0200

    adapted the changes from matlab

--- utilities/addPointLoad.m
+++ utilities/addPointLoad.m
@@ -4,8 +4,6 @@ function addPointLoad( nodeArray, modulus, direction )
 %   modulus : load modulus
 %   direction : load direction with [x, y(, z)]
 
-%%%IMPORTED new function from github
-
 for itNode = 1:length(nodeArray)
     currentNode = nodeArray(itNode);
    
@@ -15,18 +13,23 @@ for itNode = 1:length(nodeArray)
         error('the dimensions of the dofs and the load are not the same')
     end
     
+    direction = direction./norm(direction);
+    
     for itDof = 1:length(currentDofArray)
         currentDofArray(itDof).setLoad(modulus * direction(itDof));
  %       setLoad(currentDof(itDof), modulus * direction(1)); 
     end
    
+    
    
 %    currentNode.setDofValue('DISPLACEMENT_X', modulus * direction(1));
 %    currentNode.setDofValue('DISPLACEMENT_Y', modulus * direction(2));
+%
 %    if length(direction) == 3
 %        currentNode.setDofValue('DISPLACEMENT_Z', modulus * direction(3));
+%
 %    end
-end
 
+end
 end
 
