bd58063ce6cbd246d58fa09f9ca75a97bb302c2c
qaumann <qaumann@users.noreply.github.com>
2017-08-02 10:33:59 +0200

    Adding a Bossak/Newmark time integration scheme

--- core/solvers/NewmarkSolvingStrategy.m
+++ core/solvers/NewmarkSolvingStrategy.m
@@ -6,16 +6,21 @@ classdef NewmarkSolvingStrategy < Solver
     %   dynamics. John Wiley & Sons, 2014.
     
     properties (Access = private)
+        femModel
+        assembler
+        isInitialized
+        
         dt
+        alpha
         beta
         gamma
-        femModel
-        isInitialized = false
-        assembler
+        newmarkCoefficients
         
         massMatrix
         dampingMatrix
         stiffnessMatrix
+        
+        lhs
     end
     
     methods
@@ -25,21 +30,22 @@ classdef NewmarkSolvingStrategy < Solver
             solver.femModel = femModel;
             solver.dt = dt;
             solver.assembler = SimpleAssembler(femModel);
+            solver.isInitialized = false;
             
             numvarargs = length(varargin);
-            optargs = { 0.25 0.5 };
+            optargs = { 0.25 0.5 0 };
             optargs(1:numvarargs) = varargin;
-            [solver.beta, solver.gamma] = optargs{:};
+            [solver.beta, solver.gamma, solver.alpha] = optargs{:};
         end
         
         function solve(solver)
             if ~solver.isInitialized
                 solver.initialize;
             end
-            [freeDofs, fixedDofs] = solver.femModel.getDofConstraints();
+            [~, fixedDofs] = solver.femModel.getDofConstraints();
             fixedDofIds = fixedDofs.getId();
             
-            [forceFull, force] = solver.assembler.applyExternalForces(solver.femModel);
+            [~, force] = solver.assembler.applyExternalForces(solver.femModel);
             dispOld = solver.assembler.assemble3dDofVector(solver.femModel, 'DISPLACEMENT');
             dispOld = applyVectorBoundaryConditions(dispOld, fixedDofIds)';
             velOld = solver.assembler.assemble3dValueVector(solver.femModel, 'VELOCITY');
@@ -47,17 +53,26 @@ classdef NewmarkSolvingStrategy < Solver
             accOld = solver.assembler.assemble3dValueVector(solver.femModel, 'ACCELERATION');
             accOld = applyVectorBoundaryConditions(accOld, fixedDofIds)';
             
-            %set up left hand side of the les
-            lhs = solver.massMatrix ...
-                + (solver.gamma * solver.dt) .* solver.dampingMatrix ...
-                + (solver.beta * power(solver.dt,2)) .* solver.stiffnessMatrix;
-            rhs = force' ...
-                - solver.dampingMatrix * (velOld + ((1 - solver.gamma) .* solver.dt) * accOld) ...
-                - solver.stiffnessMatrix * (dispOld + solver.dt .* velOld + ((0.5 - solver.beta) * power(solver.dt,2)) .* accOld);
-            
-            accNew = linsolve(lhs, rhs);
-            velNew = velOld + ((1 - solver.gamma) * solver.dt) .* accOld + (solver.gamma * solver.dt) .* accNew;
-            dispNew = dispOld + solver.dt .* velOld + (power(solver.dt, 2) * (0.5 - solver.beta)) .* accOld + (power(solver.dt, 2) * solver.beta) .* accNew;
+            if solver.alpha == 0
+                rhs = force' ...
+                    - solver.dampingMatrix * (velOld + ((1 - solver.gamma) .* solver.dt) * accOld) ...
+                    - solver.stiffnessMatrix * (dispOld + solver.dt .* velOld + ((0.5 - solver.beta) * power(solver.dt,2)) .* accOld);
+                
+                accNew = linsolve(solver.lhs, rhs);
+                velNew = velOld + ((1 - solver.gamma) * solver.dt) .* accOld + (solver.gamma * solver.dt) .* accNew;
+                dispNew = dispOld + solver.dt .* velOld + (power(solver.dt, 2) * (0.5 - solver.beta)) .* accOld + (power(solver.dt, 2) * solver.beta) .* accNew;
+                
+                
+            else
+                c = solver.newmarkCoefficients;
+                rhs = force' ...
+                    + solver.massMatrix * ((1 - solver.alpha) .* (dispOld .* c(1) + velOld .* c(3) + accOld .* c(4)) - solver.alpha .* accOld) ...
+                    + solver.dampingMatrix * (dispOld .* c(2) + velOld .* c(5) + accOld .* c(6));
+                
+                dispNew = linsolve(solver.lhs, rhs);
+                accNew = dispNew .* c(1) - dispOld .* c(1) - velOld .* c(3) - accOld .* c(4);
+                velNew = velOld + accOld .* (solver.dt * (1 - solver.gamma)) + accNew .* (solver.dt * solver.gamma);
+            end
             
             %write the values back to the dofs / nodes
             solver.assembler.appendValuesToDofs(solver.femModel, dispNew);
@@ -72,7 +87,7 @@ classdef NewmarkSolvingStrategy < Solver
                 "ACCELERATION_X", "ACCELERATION_Y", "ACCELERATION_Z"]);
             
             % assemble and reduce matrices
-            [freeDofs, fixedDofs] = solver.femModel.getDofConstraints();
+            [~, fixedDofs] = solver.femModel.getDofConstraints();
             fixedDofIds = fixedDofs.getId();
             
             mass = solver.assembler.assembleGlobalMassMatrix(solver.femModel);
@@ -83,7 +98,7 @@ classdef NewmarkSolvingStrategy < Solver
             solver.stiffnessMatrix = applyMatrixBoundaryConditions(stiffness, fixedDofIds);
             
             % initial acceleration
-            [forceFull, force0] = solver.assembler.applyExternalForces(solver.femModel);
+            [~, force0] = solver.assembler.applyExternalForces(solver.femModel);
             disp = solver.assembler.assemble3dDofVector(solver.femModel, 'DISPLACEMENT');
             disp0 = applyVectorBoundaryConditions(disp, fixedDofIds)';
             vel = solver.assembler.assemble3dValueVector(solver.femModel, 'VELOCITY');
@@ -92,6 +107,29 @@ classdef NewmarkSolvingStrategy < Solver
             acc0 = (solver.massMatrix) \ (force0' - solver.stiffnessMatrix * disp0 - solver.dampingMatrix * vel0);
             solver.assembler.assignValuesToNodes(solver.femModel, 'ACCELERATION', acc0, 1);
             solver.isInitialized = true;
+            
+            if solver.alpha == 0
+                %set up left hand side of the les
+                solver.lhs = solver.massMatrix ...
+                    + (solver.gamma * solver.dt) .* solver.dampingMatrix ...
+                    + (solver.beta * power(solver.dt,2)) .* solver.stiffnessMatrix;
+            else
+                %coefficients for bossak/newmark
+                solver.beta = power((1.0 - solver.alpha),2) * solver.beta;
+                solver.gamma = solver.gamma - solver.alpha;
+                c = zeros(6,1);
+                c(1) = 1 / (solver.beta * power(solver.dt,2));
+                c(2) = solver.gamma / (solver.beta * solver.dt);
+                c(3) = 1 / (solver.beta * solver.dt);
+                c(4) = 1 / (2 * solver.beta) - 1;
+                c(5) = solver.gamma / solver.beta - 1;
+                c(6) = solver.dt * 0.5 * (solver.gamma / solver.beta - 2);
+                solver.newmarkCoefficients = c;
+                
+                solver.lhs = solver.stiffnessMatrix ...
+                    + ((1 - solver.alpha) * c(1)) .* solver.massMatrix ...
+                    + c(2) .* solver.dampingMatrix;
+            end
         end
     end
     
