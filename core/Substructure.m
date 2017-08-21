0ca8c57d2e24f3010952de2691da06360de75c13
SchaalF <SchaalF@users.noreply.github.com>
2017-08-21 16:20:59 +0200

    For a new substructure the nodes are in directly increasing order. This is needed to compute the Stiffness Matrix of each substructure.

--- core/Substructure.m
+++ core/Substructure.m
@@ -4,23 +4,28 @@ classdef Substructure < FemModel
     %   used to create substructes which are later used for FETI methods
     
     properties (Access = private)
+        %nodes at the interface
         nodesIntf
     end 
     
     methods
         %constructor
-        function substructur = Substructure(nodeArray, elementArray, nodesIntf, femModelParts)
+        function substructure = Substructure(nodeArray, elementArray, nodesIntf, idt, femModelParts)
             if nargin == 0
                 super_args = {};
             elseif nargin == 2
                 super_args = {nodeArray, elementArray};
-            elseif nargin == 3
-                super_args = {nodeArray, elementArray};
             elseif nargin == 4
+                super_args = {nodeArray, elementArray};
+            elseif nargin == 5
                 super_args = {nodeArray, elementArray, femModelParts};
             end
-            substructur@FemModel(super_args{:});
-            substructur.nodesIntf = nodesIntf;
+            substructure@FemModel(super_args{:});
+            substructure.nodesIntf = [nodesIntf];
+            substructure.nodesIntf.getId;
+            
+            nodeArray.getId;
+            Substructure.checkNodeIds(nodeArray, idt);
         end    
         
         %getter
@@ -28,8 +33,32 @@ classdef Substructure < FemModel
             nodesIntf = substructur.nodesIntf;
         end
     end
+    
+    methods (Static)
+        function checkNodeIds(nodeArray, idt)
+            %functions which checks whether the node Ids in a substructure
+            %are all directly increasing (just one higher than previous
+            %node)
+            for ii = 1:(length(nodeArray)-1)
+                if nodeArray(ii+1).getId - nodeArray(ii).getId > 1
+                    Substructure.changeNodeIds(nodeArray,idt);
+                end
+            end
+            %nodeArray.getId
+        end
         
-        
+        function changeNodeIds(nodeArray, idt)
+            %functions changes the Ids of the nodes in a substructure to be
+            %directly increasing. otherwise computation of the stiffness
+            %matrix is not possible in the simple assembler.
+            
+            id = idt.getNodeId;
+            for ii = 1:length(nodeArray)
+                nodeArray(ii).setId(id+ii);
+            end
+            idt.setNodeId(id+ii);
+        end
+    end 
 end
     
 
