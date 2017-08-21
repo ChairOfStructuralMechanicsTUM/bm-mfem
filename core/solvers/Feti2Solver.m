aaa89a3451aff912104aa01dcd95cbde7e560f75
SchaalF <SchaalF@users.noreply.github.com>
2017-08-21 16:29:36 +0200

    Function doing the FETI 2 method. The Boolean Matrix and C-Matrix are calculated directly here.

--- core/solvers/Feti2Solver.m
+++ core/solvers/Feti2Solver.m
@@ -6,7 +6,65 @@ classdef Feti2Solver < FetiPreparer
     end
     
     methods (Static)
-        function [lambda, alpha] = pCG(K,f,B,R,Kinv)
+        function [lambda, alpha] = pCG(K,f,B,R,Kinv,C,W)
+            
+            %change B matrix to try...
+            B{1,1} = [0 1 zeros(1,13)
+                      0 0 1 zeros(1,12)
+                      zeros(1,7) 1 zeros(1,7)
+                      zeros(1,8) 1 zeros(1,6)
+                      zeros(1,9) 1 zeros(1,5)
+                      zeros(1,10) 1 zeros(1,4)
+                      zeros(1,11) 1 zeros(1,3)
+                      zeros(1,12) 1 zeros(1,2)
+                      zeros(1,13) 2 zeros(1,1)
+                      zeros(1,14) 2
+                      zeros(8,15)];
+
+            B{1,2} = [zeros(1,12) -1 zeros(1,5)
+                      zeros(1,13) -1 zeros(1,4)
+                      zeros(1,14) -1 zeros(1,3)
+                      zeros(1,15) -1 zeros(1,2)
+                      zeros(4,18)
+                      zeros(1,16) -2 0
+                      zeros(1,17) -2
+                      zeros(1,8) -1 zeros(1,9)
+                      zeros(1,9) -1 zeros(1,8)
+                      zeros(1,10) -1 zeros(1,7)
+                      zeros(1,11) -1 zeros(1,6)
+                      zeros(4,18)];
+                  
+            B{1,3} = [zeros(4,18)
+                      zeros(1,12) -1 zeros(1,5)
+                      zeros(1,13) -1 zeros(1,4)
+                      zeros(1,14) -1 zeros(1,3)
+                      zeros(1,15) -1 zeros(1,2)
+                      zeros(1,16) -2 0
+                      zeros(1,17) -2
+                      zeros(4,18)
+                      zeros(1,4) -1 zeros(1,13)
+                      zeros(1,5) -1 zeros(1,12)
+                      zeros(1,10) -1 zeros(1,7)
+                      zeros(1,11) -1 zeros(1,6)];
+                  
+            B{1,4} = [zeros(8,18)
+                      zeros(1,16) 2 0
+                      zeros(1,17) 2
+                      zeros(1,8) 1 zeros(1,9)
+                      zeros(1,9) 1 zeros(1,8)
+                      zeros(1,10) 1 zeros(1,7)
+                      zeros(1,11) 1 zeros(1,6)
+                      zeros(1,12) 1 zeros(1,5)
+                      zeros(1,13) 1 zeros(1,4)
+                      zeros(1,14) 1 zeros(1,3)
+                      zeros(1,15) 1 zeros(1,2)];
+            
+%                   B{1,1}
+%                   B{1,2}
+%                   B{1,3}
+%                   B{1,4}
+
+            %%%Set matrices
             %ns = number of subdomains
             ns = length(K);
             %nf = number of floating subdomains
@@ -27,6 +85,9 @@ classdef Feti2Solver < FetiPreparer
                     F = F + B{1,numSubs}*Kinv{1,numSubs}*B{1,numSubs}';
                 end
             end
+            %definitness check
+            rank(F);
+            size(F,1);
             
             %d-Matrix: sum(B*K^-1*f') from 1 to ns
             d = 0;
@@ -56,85 +117,134 @@ classdef Feti2Solver < FetiPreparer
                 end
             end
             e = preE';
-                        
             
-
+            %P-Projektor: P = I-G*(G'*G)^-1*G'
+            I = eye(size(G*G'));
+            P = I-G*inv(G'*G)*G';
             
-
-            %Lumped preconditioner
-            %F01Inv = B01*K01*B01' + B02*K02*B02';
-            %Dirichlet preconditioner
-            %Q01 = [zeros(8,11);
-            %       zeros(3, 8), K01(9:11, 9:11) - K01(1:8,9:11)'*inv(K01(1:8,1:8))*K01(1:8,9:11)];
-            %Q02 = [zeros(11,14);
-            %       zeros(3, 11), K02(12:14, 12:14) - K02(1:11,12:14)'*inv(K02(1:11,1:11))*K02(1:11,12:14)];
-               
-            %F01Inv = B01*Q01*B01' + B02*Q02*B02';
+            %C-Matrix
+            C = [0;0;0;0;0;0;0;0;1;1;0;0;0;0;0;0;0;0];
             
+            %Lumped Preconditioner: try W*(sum(B*K*B'))*W
+            pre = 0;
+            for numSubs = 1:ns
+                pre = pre + B{1,numSubs}*K{1,numSubs}*B{1,numSubs}';
+            end
+            lumpedF = W*pre*W;
+            %lumpedF = pre;
             
-            %SECOND LEVEL
-            %From The second generation
-            %C is matrix localizing the jump equations at the corner of the
-            %problem. rectangular matrix with a number of columns equal to
-            %the number of corners in the problem mulitplied by the number
-            %of Lagrange multipliers defined at these corners. number of
-            %rows equal to interface dofs (4x4)
+            %%%Initializing
+            nomi = C'*P'*(d-F*G*inv(G'*G)*e);
+            denomi = C'*P'*F*P*C;
+            %gamma: only works for this case with only one crosspoint
+            %otherwise a more complicated equation has to be solved for
+            %gamme...
+            gamma(1) = nomi/denomi;
+            %lambda
+            lambda(:,1) = G*inv(G'*G)*e+P*C*gamma(1);
+            %residuum-w
+            w(:,1) = P'*(d-F*lambda(:,1));
             
-%             C = [0 0 0 0;
-%                  0 1 0 0;
-%                  0 0 0 0;
-%                  0 0 0 1];
-%                 
-%               gamma = (C'*P'*F01*P*C)
-% 
-%             %correct
-%             lambda(:,1) = G02*inv(G02'*G02)*(f02*R02)'+P*C*gamma;
-%             %w is the residuum
-              %correct
-%             w(:,1) = P'*((B02*Kplus*B02'+B01*inv(K01)*B01')-F01*lambda(:,1));
-
-
-%             while sum(abs(w(:,kk))) > num*10^-3
-%                 
-%                 y(:,kk) = Proj*F01Inv*w(:,kk);
-%   
-%                 if kk > 1
-%                     temp = 0;
-%                     for mm = 1:kk-1
-%                         temp = temp + ((y(:,kk)'*F01*p2(:,mm))/(p2(:,mm)'*F01*p2(:,mm)))*p2(:,mm);
-%                     end
-%                     p(:,kk) = y(:,kk)-temp;   
+            %Preconditioner Check
+            %F*lumpedF
+            
+            %%%Iterate
+            var = 0;
+            kk = 1;
+            while var < 1
+            %for kk = 1:100
+                y(:,kk) = P*lumpedF*w(:,kk);
+                
+                if kk > 1
+                    temp = 0;
+                    for mm = 1:kk-1
+                        temp = temp + ((y(:,kk)'*F*p2(:,mm))/(p2(:,mm)'*F*p2(:,mm)))*p2(:,mm);
+                    end
+                    p(:,kk) = y(:,kk)-temp;   
+                else
+                    p(:,kk) = y(:,kk);
+                end
+                
+                p(:,kk);
+                
+                nomi = -C'*P'*F*p(:,kk);
+                denomi = C'*P'*F*P*C;
+                gamma(kk) = nomi/denomi;
+                
+                p2(:,kk) = p(:,kk)+P*C*gamma(kk);
+                
+                eta(:,kk) = (p2(:,kk)'*w(:,kk))/(p2(:,kk)'*F*p2(:,kk));
+                
+                lambda(:,kk+1) = lambda(:,kk)+eta(:,kk)*p2(:,kk);
+                
+                w(:,kk+1) = w(:,kk)-eta(:,kk)*P'*F*p2(:,kk);
+                
+                for numEntries = 1:size(w,1)
+                    if abs(w(numEntries,kk)) > 10^-10
+                        var = 0;
+                        kk = kk+1;
+                        break;
+                    else
+                        var = 1;
+                    end
+                end
+            end
+            lambda = lambda(:,kk);
+           
+            %test
+%             R{1,2}'*(f{1,2}'-B{1,2}'*lambda)
+%             R{1,3}'*(f{1,3}'-B{1,3}'*lambda)
+%             R{1,4}'*(f{1,4}'-B{1,4}'*lambda)
+            
+            %is alpha general quantity or does one have to calculate it for
+            %every subdomain individually?
+%             for numSubs = 1:ns
+%                 if Kinv{1,numSubs} == 0
 %                 else
-%                     p(:,kk) = y(:,kk);
+%                     g = B{1,numSubs}*R{1,numSubs};
+%                     %fi = B{1,numSubs}*Kinv{1,numSubs}*B{1,numSubs}';
+%                     di = B{1,numSubs}*Kinv{1,numSubs}*f{1,numSubs}';
+%                     alpha{1,numSubs} = (inv(g'*g)*g')*(F*lambda-d);
 %                 end
-%                 
-%                 eqn = (C'*Proj'*F01*Proj*C)*beta == -C'*Proj'*F01*p(:,kk);
-%                 [gamma1 gamma2 gamma3 gamma4] = solve(eqn,beta);
-%                 gamma = [gamma1; gamma2; gamma3; gamma4];
-%                 
-%                 p2(:,kk) = p(:,kk)+Proj*C*gamma;
-%                 
-%                 n(:,kk) = (p2(:,kk)'*w(:,kk))/(p(:,kk)'*F01*p(:,kk));
-%                 
-%                 lambda(:,kk+1) = lambda(:,kk) + n(:,kk)*p2(:,kk);
-%                 
-%                 w(:,kk+1) = w(:,kk) - n(:,kk)*Proj'*F01*p2(:,kk);
-%                 
-%                 
-%                 %constraint check
-%                 g02 = G02'*lambda(:,kk);
-%                 test = G02'*p(:,kk);
-%                 
-%                 kk = kk+1;
 %             end
 
-%             lambda = lambda(:,kk);            
-%             alpha = (inv(G02'*G02)*G02')*(F01*lambda-B02*Kplus*f02'+B01*inv(K01)*f01');
-%             %is this possible, according to "a unified framework..." p.265
-%             %eq. 30
-%             alpha = (inv(G02'*G02)*G02')*(B02*Kplus*f02'+B01*inv(K01)*f01'-F01*lambda);
+%             alpha{1,1} = 0;
+% 
+%             di = 2*B{1,2}*Kinv{1,2}*f{1,2}'+B{1,1}*inv(K{1,1})*f{1,1}'+B{1,4}*Kinv{1,4}*f{1,4}';
+%             g = B{1,2}*R{1,2};
+%             %fi = 2*B{1,2}*Kinv{1,2}*B{1,2}'+B{1,1}*inv(K{1,1})*B{1,1}'+B{1,4}*Kinv{1,4}*B{1,4}';
+%             alpha{1,2} = (inv(g'*g)*g')*(F*lambda-di);
+%             
+%             di = 2*B{1,3}*Kinv{1,3}*f{1,3}'+B{1,1}*inv(K{1,1})*f{1,1}'+B{1,4}*Kinv{1,4}*f{1,4}';
+%             g = B{1,3}*R{1,3};
+%             %fi = 2*B{1,3}*Kinv{1,3}*B{1,3}'+B{1,1}*inv(K{1,1})*B{1,1}'+B{1,4}*Kinv{1,4}*B{1,4}';
+%             alpha{1,3} = (inv(g'*g)*g')*(F*lambda-di);
+%             
+%             di = B{1,2}*Kinv{1,2}*f{1,2}'+B{1,3}*Kinv{1,3}*f{1,3}'+2*B{1,4}*Kinv{1,4}*f{1,4}';
+%             g = B{1,4}*R{1,4};
+%             %fi = B{1,2}*Kinv{1,2}*B{1,2}'+B{1,3}*Kinv{1,3}*B{1,3}'+2*B{1,4}*Kinv{1,4}*B{1,4}';
+%             alpha{1,4} = (inv(g'*g)*g')*(F*lambda-di);
+% 
+%             
+%             
+%             alpha{1,2}
+%             alpha{1,3}
+%             alpha{1,4}
             
+            alphaI = inv(G'*G)*G'*(F*lambda-d);
+            alpha{1,1} = 0;
+            alpha{1,2} = alphaI(1:3,1);
+            alpha{1,3} = alphaI(4:6,1);
+            alpha{1,4} = alphaI(7:9,1);
             
+%             alpha{1,2}
+%             alpha{1,3}
+%             alpha{1,4}
+            
+%             kk
+%             w(:,kk-1)
+%             w(:,kk)
+%             w(:,kk+1)
             
         end
     end
