78178b5d4fdadf1d62f7c0268bb64af422514dbc
SchaalF <SchaalF@users.noreply.github.com>
2017-08-21 16:30:16 +0200

    Function preparing for the PCG for FETI 1 & 2. The Calculation of the Boolean Matrix/C-Matrix and W-Matrix is still wrong.

--- core/solvers/FetiPreparer.m
+++ core/solvers/FetiPreparer.m
@@ -29,6 +29,7 @@ classdef FetiPreparer < SimpleSolvingStrategy
                 B = cell(1,length(K));
                 %test variable for correct pseudo Inverse/Body Modes
                 test = 0;
+                C = [];
                 for it = 1:length(K)
                     if ismember(it, singulars)
                         %find pseudo Inverse/body modes for singulars
@@ -42,10 +43,12 @@ classdef FetiPreparer < SimpleSolvingStrategy
                         Kinv{1,it} = 0;
                     end
                     %create Boolean Matrix
-                    B{1,it} = FetiPreparer.createBooleanMatrix(substructures(it), intfNodes, it);
+                    [B{1,it},c] = FetiPreparer.createBooleanMatrix(substructures, intfNodes, it);
+                    C = [C;c];
                 end
                 
-                %f{1,1}
+                %find matrix W, which scales the preconditioners.
+                W = FetiPreparer.findW(substructures,intfNodes);
                 
                 %test for correct Pseudo Invers and Rigid Body Modes
                 if test ~= 2*length(singulars)
@@ -55,12 +58,86 @@ classdef FetiPreparer < SimpleSolvingStrategy
                 %all information given to the pCG is in cell array format
                 %with one row and number of columns corresponding to the
                 %number of subdomains
+                %FETI 2 Level
+                [lambda, alpha] = Feti2Solver.pCG(K,f,B,R,Kinv,C,W);
+                %FETI 1 Level
                 %[lambda, alpha] = Feti1Solver.pCG(K,f,B,R,Kinv);
-                [lambda, alpha] = Feti2Solver.pCG(K,f,B,R,Kinv);
-                           
-                %u01 = inv(K01)*(f01'+B01'*lambda);
-                %u02 = K02Plus*(f02'-B02'*lambda)+R02*alpha;
+               
                 
+                %%%Bridge
+%                 B{1,1} = [0 0 0 0 0 0 0 1 0 0 0
+%                           0 0 0 0 0 0 0 0 1 0 0
+%                           0 0 0 0 0 0 0 0 0 1 0
+%                           0 0 0 0 0 0 0 0 0 0 1];
+%                   
+%                 B{1,2} = [0 0 0 0 0 0 0 0 0 0 -1 0 0 0
+%                           0 0 0 0 0 0 0 0 0 0 0 -1 0 0
+%                           0 0 0 0 0 0 0 0 0 0 0 0 -1 0
+%                           0 0 0 0 0 0 0 0 0 0 0 0 0 -1];
+
+            %%%SixNode    
+            B{1,1} = [0 1 zeros(1,13)
+                      0 0 1 zeros(1,12)
+                      zeros(1,7) 1 zeros(1,7)
+                      zeros(1,8) 1 zeros(1,6)
+                      zeros(1,9) 1 zeros(1,5)
+                      zeros(1,10) 1 zeros(1,4)
+                      zeros(1,11) 1 zeros(1,3)
+                      zeros(1,12) 1 zeros(1,2)
+                      zeros(1,13) 2 zeros(1,1)
+                      zeros(1,14) 2
+                      zeros(8,15)];
+
+            B{1,2} = [zeros(1,12) -1 zeros(1,5)
+                      zeros(1,13) -1 zeros(1,4)
+                      zeros(1,14) -1 zeros(1,3)
+                      zeros(1,15) -1 zeros(1,2)
+                      zeros(4,18)
+                      zeros(1,16) -2 0
+                      zeros(1,17) -2
+                      zeros(1,8) -1 zeros(1,9)
+                      zeros(1,9) -1 zeros(1,8)
+                      zeros(1,10) -1 zeros(1,7)
+                      zeros(1,11) -1 zeros(1,6)
+                      zeros(4,18)];
+                  
+            B{1,3} = [zeros(4,18)
+                      zeros(1,12) -1 zeros(1,5)
+                      zeros(1,13) -1 zeros(1,4)
+                      zeros(1,14) -1 zeros(1,3)
+                      zeros(1,15) -1 zeros(1,2)
+                      zeros(1,16) -2 0
+                      zeros(1,17) -2
+                      zeros(4,18)
+                      zeros(1,4) -1 zeros(1,13)
+                      zeros(1,5) -1 zeros(1,12)
+                      zeros(1,10) -1 zeros(1,7)
+                      zeros(1,11) -1 zeros(1,6)];
+                  
+            B{1,4} = [zeros(8,18)
+                      zeros(1,16) 2 0
+                      zeros(1,17) 2
+                      zeros(1,8) 1 zeros(1,9)
+                      zeros(1,9) 1 zeros(1,8)
+                      zeros(1,10) 1 zeros(1,7)
+                      zeros(1,11) 1 zeros(1,6)
+                      zeros(1,12) 1 zeros(1,5)
+                      zeros(1,13) 1 zeros(1,4)
+                      zeros(1,14) 1 zeros(1,3)
+                      zeros(1,15) 1 zeros(1,2)];
+
+                %Vorzeichen: Müsste stimmen mit der jetzigen Vorzeichen-
+                %gebung in den Boolean Matrizen.
+                for numSubs = 1:length(substructures)
+                    if Kinv{1,numSubs} == 0
+                        u{1,numSubs} = inv(K{1,numSubs})*(f{1,numSubs}'-B{1,numSubs}'*lambda);
+                    else
+                        u{1,numSubs} = Kinv{1,numSubs}*(f{1,numSubs}'-B{1,numSubs}'*lambda)+R{1,numSubs}*alpha{1,numSubs};
+                    end
+                end
+                
+                %check Interface condition
+                B{1,1}*u{1,1}+B{1,2}*u{1,2}+B{1,3}*u{1,3}+B{1,4}*u{1,4};
 
              else
                 lenU = 0;
@@ -77,6 +154,62 @@ classdef FetiPreparer < SimpleSolvingStrategy
                     ll = ll+length(f{kk});
                 end
             end
+        end
+        
+        %find W matrix, which is diagonal and has as diagonal elements the
+        %inverse of the multiplicity of substructures an interface dof 
+        %belongs to
+        function W = findW(substructures,intfNodes)
+            nodesIntf = [];
+            nodesIntfUN = [];
+            %find all interface nodes in unique and in form where they
+            %appear multiple times.
+            for numSubs = 1:length(substructures)
+                for numSub = 1:length(substructures)
+                    nodesIntf = [nodesIntf intfNodes{numSubs,numSub}];
+                    nodesIntfUN = unique([nodesIntfUN intfNodes{numSubs,numSub}]);
+                end
+            end
+            %all interface nodes are ordered by their Ids
+            [~,idx] = sort([nodesIntfUN.getId]);
+            nodesIntfUN = nodesIntfUN(idx);
+            
+            ii = 0;
+            %iterate over all unique interface nodes and over all of their
+            %dofs. all dofs which are not fixed produce an entry of W.
+            for numNodes = 1:length(nodesIntfUN)
+                dofs = nodesIntfUN(numNodes).getDofArray;
+                for dof = 1:length(dofs)
+                    if ~dofs(dof).isFixed
+                        ii = ii+1;
+                        %see how many subdomains contain this node/dof
+                        members = ismember(nodesIntf,nodesIntfUN(numNodes));
+                        %+1, because each interface node belongs to at
+                        %least 2 substructures if it appears once in the
+                        %nodesIntf array.    
+                        val = sum(members)+1;
+                        W(ii,ii) = 1/val;
+                    end
+                end
+            end  
+            W = [0.5 zeros(1,17)
+                 0 0.5 zeros(1,16)
+                 zeros(1,2) 0.5 zeros(1,15)
+                 zeros(1,3) 0.5 zeros(1,14)
+                 zeros(1,4) 0.5 zeros(1,13)
+                 zeros(1,5) 0.5 zeros(1,12)
+                 zeros(1,6) 0.5 zeros(1,11)
+                 zeros(1,7) 0.5 zeros(1,10)
+                 zeros(1,8) 0.25 zeros(1,9)
+                 zeros(1,9) 0.25 zeros(1,8)
+                 zeros(1,10) 0.5 zeros(1,7)
+                 zeros(1,11) 0.5 zeros(1,6)
+                 zeros(1,12) 0.5 zeros(1,5)
+                 zeros(1,13) 0.5 zeros(1,4)
+                 zeros(1,14) 0.5 zeros(1,3)
+                 zeros(1,15) 0.5 zeros(1,2)
+                 zeros(1,16) 0.5 zeros(1,1)
+                 zeros(1,17) 0.5];
         end
         
         function intfNodes = findInterfaceNode(substructures)
@@ -282,14 +415,16 @@ classdef FetiPreparer < SimpleSolvingStrategy
         %the total Boolean Matrix for the substructure by adding up all the 
         %individual Boolean Matrices.
 
-        function B = createBooleanMatrix(substructure, intfNodes, it)
+        function [B, connection] = createBooleanMatrix(substructure, intfNodes, it)
 
             %all nodes of substructure
-            allNodes = substructure.getAllNodes;
+            allNodes = substructure(it).getAllNodes;
             
             %find all interface nodes for all interface configurations
             nodesIntf = [];
+            nodesIntfNU = [];
             for numSubs = 1:length(intfNodes)
+                nodesIntfNU = [nodesIntfNU intfNodes{it,numSubs}];
                 nodesIntf = unique([nodesIntf intfNodes{it,numSubs}]);
             end
             %sort interface nodes by Id
@@ -382,7 +517,52 @@ classdef FetiPreparer < SimpleSolvingStrategy
                 end
                 intfIndices1{1,numSubs} = indices1;
             end
-             
+            
+            %find positions of crosspoints dofs in Geometrie
+            ii = 0;
+            connections = [];
+            for numNodes = 1:length(nodesIntf)
+                dofs = nodesIntf(numNodes).getDofArray;
+                cons = [];
+                %only 2D
+                for dof = 1:2
+                    if ~dofs(dof).isFixed
+                        ii = ii+1;
+                        members = ismember(nodesIntfNU,nodesIntf(numNodes));
+                        if sum(members) >= 3
+                            cons = [cons ii];
+                        else
+                            cons = [cons 0];
+                        end
+                    else
+                        cons = [cons 0];
+                    end
+                end
+                connections(numNodes,:) = cons;
+            end
+            
+            %remove all zero rows
+            numRows = 1;
+            while numRows <= size(connections,1)
+                if sum(connections(numRows,:)) == 0
+                    connections = removerows(connections,'ind',numRows);
+                else 
+                    numRows = numRows+1;
+                end
+            end
+            
+            %connection is the matrix of crosspoints for every
+            %substructure, they are assembeled in the method calling this
+            %function
+            connection = zeros(numIntfDofs,size(connections,1));
+            %numRows and numColumns refers to the matrix connection
+            for numRows = 1:size(connections,1)
+                for numColumns = 1:size(connections,2)
+                    
+                    connection(connections(numRows,numColumns),numRows) = 1;
+                end
+            end
+
             %number of interface nodes in the total system before this 
             %substructure. meaning all the interface nodes of the
             %subdomains preceding this one.
@@ -438,13 +618,12 @@ classdef FetiPreparer < SimpleSolvingStrategy
                             for numIntfDof = 1:length(intfDofs2)
                                 d(intfDofs1(numIntfDof), intfDofs2(numIntfDof)) = 1;
                             end
-                    else
-                        %include case where there is a crosspoint!!!
                     end
                 end
                 c{1,numSubs} = d;
             end
 
+            
             %now a matrix b, which is the Boolean Matrix for each interface 
             %configuration of the whole subdomain, is created and saved for
             %each configuration in a cell array.
