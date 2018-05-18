d946b7d31aac87d60a8737296a65b96803b49946
qaumann <qaumann@users.noreply.github.com>
2018-05-18 14:35:56 +0200

    Moving stuff to the new example folder

---
+++ examples/Bridge_with_inputFile.m
@@ -0,0 +1,20 @@
+io = ModelIO('tests/validation_bridge_input.msh');
+model = io.readModel;
+model.getAllNodes.addDof({'DISPLACEMENT_X', 'DISPLACEMENT_Y', 'DISPLACEMENT_Z'});
+model.getModelPart('fixed_support').fixDof('DISPLACEMENT_X');
+model.getModelPart('fixed_support').fixDof('DISPLACEMENT_Y');
+model.getModelPart('roller_support').fixDof('DISPLACEMENT_Y');
+model.getAllNodes.fixDof('DISPLACEMENT_Z');
+
+addPointLoad(model.getNodes([3 5 9 11]),10,[0 -1 0]);
+addPointLoad(model.getNode(7),16,[0 -1 0]);
+
+solver = SimpleSolvingStrategy(model);
+solver.solve();
+
+actualDisplacementX = model.getAllNodes.getDofValue('DISPLACEMENT_X');
+actualDisplacementY = model.getAllNodes.getDofValue('DISPLACEMENT_Y');
+
+createOutputFile(model);
+
+            
