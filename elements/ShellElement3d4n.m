266df87990f9726f1e0c4161e72bf4d1166303d6
qaumann <qaumann@users.noreply.github.com>
2018-05-23 17:44:39 +0200

    Adding dof reordering

--- elements/ShellElement3d4n.m
+++ elements/ShellElement3d4n.m
@@ -25,10 +25,10 @@ classdef ShellElement3d4n < QuadrilateralElement
             
             % call the super class contructor
             shellElement3d4n@QuadrilateralElement(super_args{:});
-%             shellElement3d4n.dofNames = cellstr([ ...
-%                 "DISPLACEMENT_X", "DISPLACEMENT_Y", "DISPLACEMENT_Z", "ROTATION_X",  "ROTATION_Y",  "ROTATION_Z"]);
             shellElement3d4n.dofNames = cellstr([ ...
-                "DISPLACEMENT_Z", "ROTATION_X",  "ROTATION_Y"]);
+                "DISPLACEMENT_X", "DISPLACEMENT_Y", "DISPLACEMENT_Z", "ROTATION_X",  "ROTATION_Y",  "ROTATION_Z"]);
+%                 "DISPLACEMENT_X", "DISPLACEMENT_Y",  "ROTATION_Z"]);
+%                 "DISPLACEMENT_Z", "ROTATION_X",  "ROTATION_Y"]);
         
         end
         
@@ -58,7 +58,7 @@ classdef ShellElement3d4n < QuadrilateralElement
         
         
         
-        function [J, B_mem, B_b] = computeShapeFunction(shellElement3d4n, xi, eta)
+        function [J, B_mem, B_b] = computeShapeFunction(obj, xi, eta)
             
 %             % Shape Function and Derivatives                    
 %             N = [(1-xi)*(1-eta)/4    (1+xi)*(1-eta)/4    (1+xi)*(1+eta)/4    (1-xi)*(1+eta)/4];  
@@ -69,8 +69,8 @@ classdef ShellElement3d4n < QuadrilateralElement
             % Coordinates of the nodes forming one element 
             ele_coords = zeros(4,2); 
             for i=1:4
-                ele_coords(i,1) = shellElement3d4n.nodeArray(i).getX;
-                ele_coords(i,2) = shellElement3d4n.nodeArray(i).getY;
+                ele_coords(i,1) = obj.nodeArray(i).getX;
+                ele_coords(i,2) = obj.nodeArray(i).getY;
             end
             
             % Jacobian and inverse Jacobian
@@ -83,6 +83,42 @@ classdef ShellElement3d4n < QuadrilateralElement
                                     0, -(eta/2 - 1/2)*((3*xi^2)/4 - 3/4),                                 0, (eta/2 - 1/2)*(xi/2 - (3*xi^2)/4 + 1/4),                         0, (eta/2 - 1/2)*((3*xi^2)/4 - 3/4),                                 0, -(eta/2 - 1/2)*((3*xi^2)/4 + xi/2 - 1/4),                           0, -(eta/2 + 1/2)*((3*xi^2)/4 - 3/4),                                 0, (eta/2 + 1/2)*((3*xi^2)/4 + xi/2 - 1/4),                         0, (eta/2 + 1/2)*((3*xi^2)/4 - 3/4),                                 0, -(eta/2 + 1/2)*(xi/2 - (3*xi^2)/4 + 1/4);
                                     0, - xi^3/8 + (3*xi)/8 - 1/4,                                        0, - xi^3/8 + xi^2/8 + xi/8 - 1/8,                                0, xi^3/8 - (3*xi)/8 - 1/4,                                        0, - xi^3/8 - xi^2/8 + xi/8 + 1/8,                                 0, - xi^3/8 + (3*xi)/8 + 1/4,                                         0, xi^3/8 + xi^2/8 - xi/8 - 1/8,                                0, xi^3/8 - (3*xi)/8 + 1/4,                                         0, xi^3/8 - xi^2/8 - xi/8 + 1/8]; 
                                       
+            MN_Diff_2 = zeros(4,16);
+            MN_Diff_2(1,:) = [obj.dM_dx(1)*obj.Nx(1,eta) 0 ...
+                obj.dM_dx(1) * -1*obj.Nx(2,eta) 0 ...
+                obj.dM_dx(2) * obj.Nx(1,eta) 0 ...
+                obj.dM_dx(2) * -1*obj.Nx(2,eta) 0 ...
+                obj.dM_dx(2) * obj.Nx(3,eta) 0 ...
+                obj.dM_dx(2) * -1*obj.Nx(4,eta) 0 ...
+                obj.dM_dx(1) * obj.Nx(3,eta) 0 ...
+                obj.dM_dx(1) * -1*obj.Nx(4,eta) 0 ];
+            MN_Diff_2(2,:) = [obj.Mx(1,xi)*obj.dNx_dx(1,eta) 0 ...
+                obj.Mx(1,xi) * -1*obj.dNx_dx(2,eta) 0 ...
+                obj.Mx(2,xi) * obj.dNx_dx(1,eta) 0 ...
+                obj.Mx(2,xi) * -1*obj.dNx_dx(2,eta) 0 ...
+                obj.Mx(2,xi) * obj.dNx_dx(3,eta) 0 ...
+                obj.Mx(2,xi) * -1*obj.dNx_dx(4,eta) 0 ...
+                obj.Mx(1,xi) * obj.dNx_dx(3,eta) 0 ...
+                obj.Mx(1,xi) * -1*obj.dNx_dx(4,eta) 0 ];
+            MN_Diff_2(3,:) = [ 0 obj.Mx(1,eta) * obj.dNx_dx(1,xi) ...
+                0 obj.Mx(1,eta) * obj.dNx_dx(2,xi) ...
+                0 obj.Mx(1,eta) * obj.dNx_dx(3,xi) ...
+                0 obj.Mx(1,eta) * obj.dNx_dx(4,xi) ...
+                0 obj.Mx(2,eta) * obj.dNx_dx(3,xi) ...
+                0 obj.Mx(2,eta) * obj.dNx_dx(4,xi) ...
+                0 obj.Mx(2,eta) * obj.dNx_dx(1,xi) ...
+                0 obj.Mx(2,eta) * obj.dNx_dx(2,xi) ...
+            ];
+            MN_Diff_2(4,:) = [ 0 obj.dM_dx(1) * obj.Nx(1,xi) ...
+                0 obj.dM_dx(1) * obj.Nx(2,xi) ...
+                0 obj.dM_dx(1) * obj.Nx(3,xi) ...
+                0 obj.dM_dx(1) * obj.Nx(4,xi) ...
+                0 obj.dM_dx(2) * obj.Nx(3,xi) ...
+                0 obj.dM_dx(2) * obj.Nx(4,xi) ...
+                0 obj.dM_dx(2) * obj.Nx(1,xi) ...
+                0 obj.dM_dx(2) * obj.Nx(2,xi) ...
+            ];
+                                
             % Displacement Transformation Matrix
             Tr = sparse(16,12); 
             Tr(1,1) = 1; 
@@ -110,6 +146,7 @@ classdef ShellElement3d4n < QuadrilateralElement
             
             % Computing the B_Membrane Matrix with a drilling dof
             B_mem = A * inv_J_TR * MN_Diff_Par * Tr; 
+            B_mem = A * inv_J_TR * MN_Diff_2 * Tr; 
 
             % Assembling the Discrete Kirchhoff Shape Function Matrix
             Psi_Diff_Par = zeros(4,12); 
@@ -150,7 +187,7 @@ classdef ShellElement3d4n < QuadrilateralElement
             Psi_Diff_Par(1,11) = 0.375 * (((- 2 * xi * (1 + eta) * (ele_coords(3,1) - ele_coords(4,1)) * (ele_coords(3,2) - ele_coords(4,2))) / ((ele_coords(3,1) - ele_coords(4,1))^2 + (ele_coords(3,2) - ele_coords(4,2))^2)) ...
                                         - (((1 - eta^2) * (ele_coords(4,1) - ele_coords(1,1)) * (ele_coords(4,2) - ele_coords(1,2))) / ((ele_coords(1,1) - ele_coords(4,1))^2 + (ele_coords(1,2) - ele_coords(4,2))^2)));
 
-            Psi_Diff_Par(1,12) = 0.25 * ((xi - 1) * (eta + 1) + (eta + 1) * (xi - eta + 1)) ... 
+            Psi_Diff_Par(1,12) = 0.25 * ((xi - 1) * (eta + 1) + (eta + 1) * (eta - xi + 1)) ... 
                                         + ((xi * (1 + eta) * (0.25 * (ele_coords(3,1) - ele_coords(4,1))^2 - 0.5 * (ele_coords(3,2) - ele_coords(4,2))^2  )) / ((ele_coords(3,1) - ele_coords(4,1))^2 + (ele_coords(3,2) - ele_coords(4,2))^2)) ...
                                             + 0.5 * ( ((1- eta^2) * (0.25 * (ele_coords(1,1) - ele_coords(4,1))^2 - 0.5 * (ele_coords(1,2) - ele_coords(4,2))^2  )) / ((ele_coords(1,1) - ele_coords(4,1))^2 + (ele_coords(1,2) - ele_coords(4,2))^2)); 
 
@@ -295,48 +332,53 @@ classdef ShellElement3d4n < QuadrilateralElement
            
             [w,g] = returnGaussPoint(nr_gauss_points);
             
-            stiffnessMatrixMemb = sparse (12,12);
-            stiffnessMatrixBend = sparse (12,12);
-            stiffnessMatrix = sparse(12,12);
-
-%             for xi = 1 : nr_gauss_points
-%                 for eta = 1 : nr_gauss_points
-%                 [J, B_mem, B_b] = computeShapeFunction(shellElement3d4n,g(xi),g(eta));
-% 
-%                 
-%                     stiffnessMatrixMemb = stiffnessMatrixMemb +             ...
-%                                                 thickness* B_mem' * D_mem * B_mem * det(J) * w(xi) * w(eta);
-%                            
-%                     stiffnessMatrixBend = stiffnessMatrixBend +             ...
-%                                                 B_b' * D_b * B_b * det(J) * w(xi) * w(eta);       
-%                 
-%                 end
-%             end
-%             stiffnessMatrix = sparse([stiffnessMatrixMemb, zeros(12,12); zeros(12,12), stiffnessMatrixBend]);
-
+            stiffnessMatrixMemb = zeros (12,12);
+            stiffnessMatrixBend = zeros (12,12);
+            stiffnessMatrix = zeros(24,24);
 
             for xi = 1 : nr_gauss_points
-                    for eta = 1 : nr_gauss_points
-                         [J, ~, B_b] = computeShapeFunction(shellElement3d4n,g(xi),g(eta));
+                for eta = 1 : nr_gauss_points
+                [J, B_mem, B_b] = computeShapeFunction(shellElement3d4n,g(xi),g(eta));
 
-                         stiffnessMatrixBend = stiffnessMatrixBend +             ...
-                                                    B_b' * D_b * B_b * det(J) * w(xi) * w(eta);               
-                    end
+                
+                    stiffnessMatrixMemb = stiffnessMatrixMemb +             ...
+                                                thickness.* B_mem' * D_mem * B_mem * det(J) * w(xi) * w(eta);
+                           
+                    stiffnessMatrixBend = stiffnessMatrixBend +             ...
+                                                B_b' * D_b * B_b * det(J) * w(xi) * w(eta);       
+                
+                end
             end
-              
-            stiffnessMatrix = stiffnessMatrixBend;
+%             stiffnessMatrix = sparse([stiffnessMatrixMemb, zeros(12,12); zeros(12,12), stiffnessMatrixBend]);
+            % assemble stiffness matrices
+            stiffnessMatrix([1 2 6 7 8 12 13 14 18 19 20 24],[1 2 6 7 8 12 13 14 18 19 20 24]) = stiffnessMatrixMemb;
+            stiffnessMatrix([3 4 5 9 10 11 15 16 17 21 22 23],[3 4 5 9 10 11 15 16 17 21 22 23]) = stiffnessMatrixBend;
+%             stiffnessMatrix2(:,1) = stiffnessMatrixMemb(:,1);
+
+%             stiffnessMatrix = stiffnessMatrixMemb;
+%             for xi = 1 : nr_gauss_points
+%                     for eta = 1 : nr_gauss_points
+%                          [J, ~, B_b] = computeShapeFunction(shellElement3d4n,g(xi),g(eta));
+% 
+%                          stiffnessMatrixBend = stiffnessMatrixBend +             ...
+%                                                     B_b' * D_b * B_b * det(J) * w(xi) * w(eta);               
+%                     end
+%             end
+%               
+%             stiffnessMatrix = stiffnessMatrixBend;
             
         end
         
-%         function dofs = getDofList(element)
-%             dofs([1  7  13 19]) = element.nodeArray.getDof('DISPLACEMENT_X');
-%             dofs([2  8  14 20]) = element.nodeArray.getDof('DISPLACEMENT_Y');
-%             dofs([4 10 16 22]) = element.nodeArray.getDof('DISPLACEMENT_Z');
-%             dofs([5 11 17 23]) = element.nodeArray.getDof('ROTATION_X');
-%             dofs([6 12 18 24]) = element.nodeArray.getDof('ROTATION_Y');
-%             dofs([3  9  15 21]) = element.nodeArray.getDof('ROTATION_Z');
-% 
-%         end
+        function dofs = getDofList(element)
+            dofs([1  7  13 19]) = element.nodeArray.getDof('DISPLACEMENT_X');
+            dofs([2  8  14 20]) = element.nodeArray.getDof('DISPLACEMENT_Y');
+            dofs([3  9  15 21]) = element.nodeArray.getDof('DISPLACEMENT_Z');
+            dofs([4 10 16 22]) = element.nodeArray.getDof('ROTATION_X');
+            dofs([5 11 17 23]) = element.nodeArray.getDof('ROTATION_Y');
+            dofs([6 12 18 24]) = element.nodeArray.getDof('ROTATION_Z');
+            
+
+        end
 %         
 %         function vals = getValuesVector(element, step)
 %             vals = zeros(1,24);
@@ -348,19 +390,96 @@ classdef ShellElement3d4n < QuadrilateralElement
 %             vals([3  9  15 21]) = element.nodeArray.getDofValue('ROTATION_Z',step);
 %         end
         
-         function dofs = getDofList(element)
-            dofs([1 4 7 10]) = element.nodeArray.getDof('DISPLACEMENT_Z');
-            dofs([2 5 8 11]) = element.nodeArray.getDof('ROTATION_X');
-            dofs([3 6 9 12]) = element.nodeArray.getDof('ROTATION_Y');
-        end
+%          function dofs = getDofList(element)
+%              dofs([1 7 13 19]) = element.nodeArray.getDof('DISPLACEMENT_X');
+%              dofs([2 8 14 20]) = element.nodeArray.getDof('DISPLACEMENT_Y');
+%              dofs([3 9 15 21]) = element.nodeArray.getDof('ROTATION_Z');
+%              dofs([4 10 16 22]) = element.nodeArray.getDof('DISPLACEMENT_Z');
+%              dofs([5 11 17 23]) = element.nodeArray.getDof('ROTATION_Y');
+%              dofs([6 12 18 24]) = element.nodeArray.getDof('ROTATION_X');
+%          end
+        
+%          function dofs = getDofList(element)
+%             dofs([1 4 7 10]) = element.nodeArray.getDof('DISPLACEMENT_Z');
+%             dofs([2 5 8 11]) = element.nodeArray.getDof('ROTATION_X');
+%             dofs([3 6 9 12]) = element.nodeArray.getDof('ROTATION_Y');
+% end
+
+%          function dofs = getDofList(element)
+%             dofs([1 4 7 10]) = element.nodeArray.getDof('DISPLACEMENT_X');
+%             dofs([2 5 8 11]) = element.nodeArray.getDof('DISPLACEMENT_Y');
+%             dofs([3 6 9 12]) = element.nodeArray.getDof('ROTATION_Z');
+% end
         
         function vals = getValuesVector(element, step)
             vals = zeros(1,12);
             
-            vals([1 4 7 10]) = element.nodeArray.getDofValue('DISPLACEMENT_Z',step);
-            vals([2 5 8 11]) = element.nodeArray.getDofValue('ROTATION_X',step);
-            vals([3 6 9 12]) = element.nodeArray.getDofValue('ROTATION_Y',step);
+%             vals([1 4 7 10]) = element.nodeArray.getDofValue('DISPLACEMENT_Z',step);
+%             vals([2 5 8 11]) = element.nodeArray.getDofValue('ROTATION_X',step);
+%             vals([3 6 9 12]) = element.nodeArray.getDofValue('ROTATION_Y',step);
         end
+    end
+    
+    methods (Access = private, Static = true)
+        
+%         function dMN_dxi(r,s,xi,eta)
+%             if (r == 1) && (s == 1)
+%                 
+%             end
+%         end
+        
+        function res = Mx(i,x)
+            switch i
+                case 1
+                    res = 0.5 * (1-x);
+                case 2
+                    res = 0.5 * (1+x);
+                otherwise
+                    error('Mx only valid with 1 or 2, got %f',i)
+            end
+        end
+        
+        function res = dM_dx(i)
+            switch i
+                case 1
+                    res = -0.5;
+                case 2
+                    res = 0.5;
+                otherwise
+                    error('dM_d only valid with 1 or 2, got %f',i)
+            end
+        end
+        
+        function res = Nx(i,x)
+            switch i
+                case 1
+                    res = 0.5 - 0.75*x + 0.25*x^3;
+                case 2
+                    res = 0.25 - 0.25*x - 0.25*x^2 + 0.25*x^3;
+                case 3
+                    res = 0.5 + 0.75*x - 0.25*x^3;
+                case 4
+                    res = - 0.25 - 0.25*x + 0.25*x^2 + 0.25*x^3;
+                otherwise
+                    error('Nx only valid with 1,2,3 or 4, got %f',i)
+            end
+        end
+        
+        function res = dNx_dx(i,x)
+            switch i
+                case 1
+                    res = -0.75 + 0.75 * x^2;
+                case 2
+                    res = -0.25 - 0.5*x + 0.75*x^2;
+                case 3
+                    res = 0.75 - 0.75*x^2;
+                case 4
+                    res = -0.25 + 0.5*x + 0.75*x^2;
+                otherwise
+                    error('dNx_dx only valid with 1,2,3 or 4, got %f',i)
+            end
+        end
+        
     end
 end
 
