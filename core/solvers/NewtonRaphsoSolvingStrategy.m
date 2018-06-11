e6f2134cbdf0ebc794de342c9739596e9baca2a6
dagmawibekel <32930605+dagmawibekel@users.noreply.github.com>
2018-06-11 10:08:33 +0200

    Copy of simplesolvingstrategy to be modified

---
+++ core/solvers/NewtonRaphsoSolvingStrategy.m
@@ -0,0 +1,52 @@
+classdef NewtonRaphsoSolvingStrategy < Solver
+    %SIMPLESOLVINGSTRATEGY Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (Access = private)
+        femModel
+        isInitialized
+    end
+    
+    
+    
+    methods
+        
+        %constructor
+        function newtonRaphsonSolver = NewtonRaphsonSolvingStrategy(femModel)
+            if (nargin > 0)
+               newtonRaphsonSolver.femModel = femModel;
+               newtonRaphsonSolver.isInitialized = false;
+            end
+        end
+        
+        function x = solve(newtonRaphsonSolver)
+            if ~ newtonRaphsonSolver.isInitialized
+                newtonRaphsonSolver.initialize();
+            end
+            
+            [~, Kred] = SimpleAssembler.assembleGlobalStiffnessMatrix(simpleSolver.femModel);
+            [~, fred] = SimpleAssembler.applyExternalForces(simpleSolver.femModel);
+            
+            x = linsolve(Kred, fred.');
+            
+            SimpleAssembler.assignResultsToDofs(simpleSolver.femModel, x);
+        end
+        
+        function initialize(simpleSolver)
+            simpleSolver.femModel.initialize;
+            simpleSolver.isInitialized = true;
+        end
+        
+        function nodalForces = getNodalForces(simpleSolver, step)
+            nodalForces = SimpleAssembler.applyExternalForces(simpleSolver.femModel);
+            [~, fixedDofs] = simpleSolver.femModel.getDofConstraints();
+            
+            K = SimpleAssembler.assembleGlobalStiffnessMatrix(simpleSolver.femModel);
+            nodalForces(fixedDofs.getId) = K(fixedDofs.getId, :) ...
+                * simpleSolver.femModel.getDofArray.getValue(step);
+            
+        end
+        
+    end
+end
+
