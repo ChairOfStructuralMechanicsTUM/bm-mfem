bae08bc0d855709a3dcedcc18e9ea560ba5053e9
qaumann <qaumann@users.noreply.github.com>
2018-07-09 14:38:50 +0200

    adding input tests

--- tests/VariousTests.m
+++ tests/VariousTests.m
@@ -8,8 +8,8 @@ classdef VariousTests <  matlab.unittest.TestCase
     methods (Test)
         
         function elementPropertyTest(testCase)
-            %ELEMENTPROPERTYTEST tests the correct assignment and copy
-            %behavior of properties assigned to an element
+        %ELEMENTPROPERTYTEST tests the correct assignment and copy behavior
+        %of properties assigned to an element
             model = FemModel();
             model.addNewNode(1,0,0,0);
             model.addNewNode(2,1,0,0);
@@ -32,12 +32,28 @@ classdef VariousTests <  matlab.unittest.TestCase
             testCase.verifyEqual(ele02.getPropertyValue('YOUNGS_MODULUS'), 2)
         end
         
-        function checkConvexityTest(testCase)
+        function checkConvexityTestQuads(testCase)
+        %CHECKCONVEXITYTEST tests the convexity check for quads
             nodeArray = [Node(1,0,0) Node(3,2,1) Node(2,2,0) Node(4,0,1)];
             nodeArray.addDof({'DISPLACEMENT_X', 'DISPLACEMENT_Y'});
             
             testCase.verifyError(@() QuadrilateralElement2d4n(1,nodeArray), ...
                 'MATLAB:bm_mfem:elementNotConvex');
+        end
+        
+        function mdpaInputTest(testCase)
+        %MDPAINPUTTEST tests the input from a mdpa file using properties,
+        %line comments, different element types, and model parts
+            import matlab.unittest.constraints.IsTrue
+            
+            io = MdpaInput('tests/mdpatest.mdpa');
+            model = io.readModel;
+            
+            testCase.verifyEqual(model.getNode(14).getX,2.213);
+            testCase.verifyEqual(model.getElement(9).getPropertyValue('DENSITY'),7850);
+            testCase.verifyEqual(model.getElement(11).getPropertyValue('POISSON_RATIO'),0.3);
+            testCase.verifyThat(isa(model.getElement(11),'ReissnerMindlinElement3d4n'),IsTrue);
+            testCase.verifyEqual(model.getModelPart('PointLoad').nodes.getId(),11);
         end
     end
     
