5a5308f8ca2835df72d118af044c9740746679ea
SebastianSiegel <SebastianSiegel@users.noreply.github.com>
2018-07-31 13:01:39 +0200

    regroup

--- core/substructure1.m
+++ core/substructure1.m
@@ -1,4 +1,5 @@
-function [Kmatrix]=substructuretest1 (N)
+function [Kmatrix]=substructure1 (N)
+%N: Anzahl der gewünschten substructures
 %Test-steifigkeitsmatrix wird eingeführt um substrrukturierung testen zu
 %können
 %dof: [D1 D2 D3]^T werden an K ranmultipliziert: erste Spalte von K bezieht
@@ -9,15 +10,17 @@ function [Kmatrix]=substructuretest1 (N)
 
 %Kmatrix= globale Systemsteifigkeitsmatrix, lokale Elemente bereits
 %assembliert
-%System: Fachwerk mit Grundelement 1; 7 Elemente 6 Knoten 12 globalen Dofs
-Kmatrix=string(ones(12));
-%id=string();
-for i=1:12
-    for j=1:12
-        id=[i,j]
-        Kmatrix(i,j)=['K' num2str(id(1)) num2str(id(2))]
-    end
-end
+%System: Fachwerk mit Grundelement 1; 12 Elemente 9 Knoten 18 globalen Dofs
+%%
+%Kmatrix=string(ones(18));
+% for i=1:18
+%     for j=1:18
+%         id=[i,j]
+%         Kmatrix(i,j)=['K' num2str(id(1)) num2str(id(2))]
+%     end
+% end
+
+%%
 
 
 end
---
+++ tests/substructuresetup.m
@@ -0,0 +1,27 @@
+%testscript um substructure function zu testen
+
+%%
+%N: Anzahl der gewünschten substructures
+%Test-steifigkeitsmatrix wird eingeführt um substrrukturierung testen zu
+%können
+%dof: [D1 D2 D3]^T werden an K ranmultipliziert: erste Spalte von K bezieht
+%sich auf D1... 
+%Benennung von K: Kik: Festhaltekraft am Freiheitsgrad(dof)
+%i infolge Einheitsverschiebung Dk=1
+
+
+%Kmatrix= globale Systemsteifigkeitsmatrix, lokale Elemente bereits
+%assembliert
+%System: Fachwerk mit Grundelement 1; 12 Elemente 9 Knoten 18 globalen Dofs
+
+%%
+Kmatrix=string(ones(18));
+for i=1:18
+    for j=1:18
+        id=[i,j]
+        Kmatrix(i,j)=['K' num2str(id(1)) num2str(id(2))]
+    end
+end
+%%
+
+
