9c7e92b89adcc8db4aafe8de65e243bb509aa204
qaumann <qaumann@users.noreply.github.com>
2018-07-31 13:53:46 +0200

    Adjusting tests

--- tests/ElementTests.m
+++ tests/ElementTests.m
@@ -372,7 +372,7 @@ classdef ElementTests < matlab.unittest.TestCase
             solver.solve(5);
             
             %eigenfrequencies in Hz
-            actualEigenfrequencies = solver.getEigenfrequencies ./ (2*pi);
+            actualEigenfrequencies = solver.getEigenfrequencies('Hz');
             expectedEigenfrequencies = [1.01402840311520;3.22165476249304;3.22165476249320;...
                 5.32248978160243;10.5674305646586];
             
@@ -611,7 +611,7 @@ classdef ElementTests < matlab.unittest.TestCase
             solver.solve(5);
             
             % Assertion
-            actualEigenfrequencies = sort(solver.getEigenfrequencies);
+            actualEigenfrequencies = solver.getEigenfrequencies('Hz');
             expectedEigenfrequencies = [41.3911366690480;80.2659687320295;...
                 80.2659687320296;103.127323365447;122.970895447928];
             
@@ -731,7 +731,7 @@ classdef ElementTests < matlab.unittest.TestCase
             solver.solve(5);
             
             % Assertion
-            actualEigenfrequencies = sort(solver.getEigenfrequencies);
+            actualEigenfrequencies = solver.getEigenfrequencies('Hz');
             expectedEigenfrequencies = [46.1882586911365;103.709269817444;...
                 103.709269817444;154.684851632199;197.188070901913];
             
