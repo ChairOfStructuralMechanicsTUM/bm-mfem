5e42f0d3ad9ff0590e227ded48c53f7f9020dd2e
Johannes <Mr_johannes.schauer@web.de>
2018-08-03 17:41:07 +0200

    Wochenende

--- rechteckigerEinschluss2.m
+++ rechteckigerEinschluss2.m
@@ -5,14 +5,67 @@ io=MdpaInput('Bsp_rechteckigerEinschluss_mK_3D.mdpa'); %specify input file
 model = io.readModel(); %read the model
 
 model.getAllNodes.addDof(["DISPLACEMENT_X","DISPLACEMENT_Y"]);
-a=model.getAllModelParts
+% a=model.getAllModelParts
 
-leftNodes = model.getModelPart('GENERIC_leftNodes').getNodes()
-leftNodes2 = model.getModelPart('GENERIC_leftNodes2').getNodes()
-rightNodes = model.getModelPart('GENERIC_rightNodes').getNodes()
-rightNodes2 = model.getModelPart('GENERIC_rightNodes2').getNodes()
-innerNodes = model.getModelPart('GENERIC_innerNodes').getNodes()
-innerNodes2 = model.getModelPart('GENERIC_innerNodes2').getNodes()
+% leftNodes = model.getModelPart('GENERIC_leftNodes').getNodes()
+% leftNodes2 = model.getModelPart('GENERIC_leftNodes2').getNodes()
+% rightNodes = model.getModelPart('GENERIC_rightNodes').getNodes()
+% rightNodes2 = model.getModelPart('GENERIC_rightNodes2').getNodes()
+% innerNodes = model.getModelPart('GENERIC_innerNodes').getNodes()
+% innerNodes2 = model.getModelPart('GENERIC_innerNodes2').getNodes()
+
+
+
+
+model.getAllElements.setPropertyValue('YOUNGS_MODULUS',7e10);
+model.getAllElements.setPropertyValue('POISSON_RATIO',0.34);
+model.getAllElements.setPropertyValue('NUMBER_GAUSS_POINT',2);
+model.getAllElements.setPropertyValue('DENSITY',2699);
+
+
+solver = BlochInverse1D(model);
+assembling = SimpleAssembler(model);
+
+stiffnessMatrix = assembling.assembleGlobalStiffnessMatrix(model);
+            
+massMatrix = assembling.assembleGlobalMassMatrix(model);
+
+
+initialize(solver)
+
+
+[Ksorted,Msorted] = sortKandM(solver,stiffnessMatrix,massMatrix);
+
+numberOfPhases = 20;
+
+[Kred,Mred] = reducedStiffnesAndMass (solver,Ksorted,Msorted,numberOfPhases);  
+
+omega = cell(numberOfPhases,1);
+
+nob = 10;
+[kx,miu] = propConst(solver,numberOfPhases); %already used in reducedStiffnesAndMass(..)
+
+    
+for j = 1:nob
+%     f(j,1) = zeros(1,numberOfPhases); 
+    for i = 1:numberOfPhases
+        omega{i,1} = solver.calcOmega(Kred{i,1},Mred{i,1},nob);
+        f(j,i) = omega{i,1}(j,1)/(2*pi);
+
+    end
+
+    figure(1)
+    plot(kx,f(j,:),'r')
+    title('Dispersion curves')
+    xlabel('Phase k')
+    ylabel('frequenzy f')
+    xlim([0 pi])
+    legend(['bandnumber: ' num2str(j)],'Location','EastOutside')
+    hold on
+    
+
+end
+
 
 % % %set boundary conditions for all elements in 'left_support' and 'right_support':
 % % model.getModelPart('GENERIC_left_support').getNodes().fixAllDofs();
