35c1a3912bd5fca0eb0c9cdc126d0ad62de3a173
franzi-mittermeier <franzi-mittermeier@users.noreply.github.com>
2018-08-07 15:55:10 +0200

    testing Bloch solver - further adaptions needed

--- examples/bloch_gidinput_test.m
+++ examples/bloch_gidinput_test.m
@@ -1,12 +1,14 @@
 clear
 close all
 
-io=MdpaInput('Bsp_rechteckigerEinschluss_mK_3D.mdpa'); %specify input file
+io=MdpaInput('Cell_Input.mdpa'); %specify input file
 model = io.readModel(); %read the model
 
 model.getAllNodes.addDof(["DISPLACEMENT_X","DISPLACEMENT_Y"]);
 
-% leftNodes = model.getModelPart('GENERIC_leftNodes').getNodes()
+innerNodes = model.getModelPart('GENERIC_Inner_Points').getNodes()
+id = getId(innerNodes) 
+coords = getCoords(innerNodes)
 % leftNodes2 = model.getModelPart('GENERIC_leftNodes2').getNodes()
 % rightNodes = model.getModelPart('GENERIC_rightNodes').getNodes()
 % rightNodes2 = model.getModelPart('GENERIC_rightNodes2').getNodes()
@@ -18,14 +20,38 @@ model.getAllElements.setPropertyValue('POISSON_RATIO',0.34);
 model.getAllElements.setPropertyValue('NUMBER_GAUSS_POINT',2);
 model.getAllElements.setPropertyValue('DENSITY',2699);
 
+model.addNewNode(244,0.1,0.075,0);
+model.addNewElement('SpringDamperElement3d2n',188,[59 244]);
+model.addNewElement('SpringDamperElement3d2n',189,[244 159]);
+model.addNewElement('ConcentratedMassElement3d1n',190, 244);
+
+model.getNode(59).addDof("DISPLACEMENT_Z");
+model.getNode(159).addDof("DISPLACEMENT_Z");
+model.getNode(244).addDof(["DISPLACEMENT_X","DISPLACEMENT_Y","DISPLACEMENT_Z"]);
+
+model.getNode(59).fixDof('DISPLACEMENT_Z');
+model.getNode(159).fixDof('DISPLACEMENT_Z');
+model.getNode(244).fixDof('DISPLACEMENT_Y');
+model.getNode(244).fixDof('DISPLACEMENT_Z');
+
+model.getElement(188).setPropertyValue('ELEMENTAL_STIFFNESS',7e5);
+model.getElement(188).setPropertyValue('ELEMENTAL_DAMPING',0);
+model.getElement(189).setPropertyValue('ELEMENTAL_STIFFNESS',7e5);
+model.getElement(189).setPropertyValue('ELEMENTAL_DAMPING',0);
+
+model.getElement(190).setPropertyValue('ELEMENTAL_MASS',7e2);
+model.getElement(190).setPropertyValue('VOLUME_ACCELERATION',10);
+
 solver = BlochInverse1D(model);
 assembling = SimpleAssembler(model);
 
-stiffnessMatrix = assembling.assembleGlobalStiffnessMatrix(model);            
-massMatrix = assembling.assembleGlobalMassMatrix(model);
+[stiffnessMatrix,Kred1] = assembling.assembleGlobalStiffnessMatrix(model);            
+[massMatrix,Mred1] = assembling.assembleGlobalMassMatrix(model);
+% stiffnessMatrix = assembling.assembleGlobalStiffnessMatrix(model);            
+% massMatrix = assembling.assembleGlobalMassMatrix(model);
 
 initialize(solver)
-[Ksorted,Msorted] = sortKandM(solver,stiffnessMatrix,massMatrix);
+[Ksorted,Msorted] = sortKandM(solver,Kred1,Mred1);
 numberOfPhases = 20;
 [Kred,Mred] = reducedStiffnesAndMass (solver,Ksorted,Msorted,numberOfPhases);  
 omega = cell(numberOfPhases,1);
@@ -47,13 +73,5 @@ for j = 1:nob
     hold on
 end
 
-% % %set boundary conditions for all elements in 'left_support' and 'right_support':
-% % model.getModelPart('GENERIC_left_support').getNodes().fixAllDofs();
-% % model.getModelPart('GENERIC_right_support').getNodes().fixAllDofs();
-% % 
-% % %set load for all elements in 'inner_circle':
-% % model.getModelPart('GENERIC_inner_circle').getNodes().setDofLoad('DISPLACEMENT_X',10000);
-% % 
-% % 
-% % v=Visualization(model); %set up visualization
-% % v.plotUndeformed()  %visualize
+v=Visualization(model); %set up visualization
+v.plotUndeformed()  %visualize
