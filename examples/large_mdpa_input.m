1a86f8d6d537b48cbdef04b4e30bc6da08521457
ga78vuk <38356971+ga78vuk@users.noreply.github.com>
2018-08-08 17:13:04 +0200

    Funktioniert NICHT
    FEM_Model durch zusätzlich eingefügten Punkt nicht lösbar. Teile der Hauptdiagonale von M und K = 0;
    
    Lösungsansatz löst dieses Problem nicht

--- examples/large_mdpa_input.m
+++ examples/large_mdpa_input.m
@@ -16,9 +16,58 @@ model.getModelPart('GENERIC_right_support').getNodes().fixAllDofs();
 ln = model.getModelPart('GENERIC_left_support').getNodes()
 rn = model.getModelPart('GENERIC_right_support').getNodes()
 %set load for all elements in 'inner_circle':
-model.getModelPart('GENERIC_inner_circle').getNodes().setDofLoad('DISPLACEMENT_X',10000);
+% model.getModelPart('GENERIC_inner_circle').getNodes().setDofLoad('DISPLACEMENT_X',10000);
 
 
 v=Visualization(model); %set up visualization
 v.plotUndeformed()  %visualize
 
+
+model.getAllElements.setPropertyValue('YOUNGS_MODULUS',7e10);
+model.getAllElements.setPropertyValue('POISSON_RATIO',0.34);
+model.getAllElements.setPropertyValue('NUMBER_GAUSS_POINT',2);
+model.getAllElements.setPropertyValue('DENSITY',2699);
+
+
+solver = BlochInverse1D(model);
+assembling = SimpleAssembler(model);
+
+stiffnessMatrix = assembling.assembleGlobalStiffnessMatrix(model);
+            
+massMatrix = assembling.assembleGlobalMassMatrix(model);
+
+
+initialize(solver)
+
+
+[Ksorted,Msorted] = sortKandM(solver,stiffnessMatrix,massMatrix);
+
+numberOfPhases = 20;
+
+[Kred,Mred] = reducedStiffnesAndMass (solver,Ksorted,Msorted,numberOfPhases);  
+
+omega = cell(numberOfPhases,1);
+
+nob = 10;
+[kx,miu] = propConst(solver,numberOfPhases); %already used in reducedStiffnesAndMass(..)
+
+    
+for j = 1:nob
+%     f(j,1) = zeros(1,numberOfPhases); 
+    for i = 1:numberOfPhases
+        omega{i,1} = solver.calcOmega(Kred{i,1},Mred{i,1},nob);
+        f(j,i) = omega{i,1}(j,1)/(2*pi);
+
+    end
+
+    figure(1)
+    plot(kx,f(j,:),'r')
+    title('Dispersion curves')
+    xlabel('Phase k')
+    ylabel('frequenzy f')
+    xlim([0 pi])
+    legend(['bandnumber: ' num2str(j)],'Location','EastOutside')
+    hold on
+    
+
+end
