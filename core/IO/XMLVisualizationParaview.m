46add4d1c1d118142e6e876708e5e739abaff345
dagmawibekel <dagmawi.bekel@gmail.com>
2018-08-21 20:59:31 +0200

    XML

--- core/IO/XMLVisualizationParaview.m
+++ core/IO/XMLVisualizationParaview.m
@@ -1,4 +1,4 @@
-classdef VisualizationParaview < handle
+classdef XMLVisualizationParaview < handle
     properties (Access = private)
         model
         filename
@@ -15,18 +15,27 @@ classdef VisualizationParaview < handle
         end
         
         function vtkWrite(obj)
-            for tstep = 2:10
+            
+            dofs = obj.model.getAllNodes.getDofMap.keys;
+            nTsteps = length(obj.model.getNode(1).getDofValue(dofs{1},'all'));
+            
+            for tstep = 1:nTsteps
                 
             % VTK Header using ASCII Format
-            fid = fopen([obj.filename num2str(time) '.vtk'], 'w');
-            fprintf(fid, '# vtk DataFile Version 3.0\n');
-            fprintf(fid, 'VTK from Matlab\n');
-            fprintf(fid, 'ASCII\nDATASET UNSTRUCTURED_GRID\n');
+            fid = fopen([num2str(tstep) obj.filename], 'w');
+            
+            nPoints = length(obj.model.getAllNodes());
+             nElements = length(obj.model.getAllElements());
+             
+             
+            fprintf(fid, ['<VTKFile type="UnstructuredGrid" version="1.0" byte_order="LittleEndian" header_type="UInt64">\n',...
+                             '\t<UnstructuredGrid>\n', ...
+                             '\t\t<Piece NumberOfPoints="%d" NumberOfCells="%d">\n ', ...
+                             '\t\t\t<PointData>\n'], nPoints, nElements);
+            
+            
             
             % Coordinates of the Vertices
-            nPoints = length(obj.model.getAllNodes());
-            fprintf(fid, 'POINTS %d float \n', nPoints);
-            
             for i = 1:nPoints
                 fprintf(fid, [num2str(obj.model.getNode(i).getCoords()) '\n']);
             end
@@ -68,7 +77,7 @@ classdef VisualizationParaview < handle
             
             % Solution Vector           
             fprintf(fid, 'POINT_DATA %d \n', nPoints);
-            fprintf(fid, 'FIELD Output %d \n', (nTsteps-1));
+            fprintf(fid, 'FIELD Output %d \n', 2);
             
 
                 disp = zeros(nPoints,3);
@@ -79,36 +88,33 @@ classdef VisualizationParaview < handle
     
                 
                 if find(ismember(obj.model.getAllNodes.getDofMap.keys, 'DISPLACEMENT_X'))
-                    disp(:,1) = obj.model.getAllNodes.getDofValue('DISPLACEMENT_X',time);
+                    disp(:,1) = obj.model.getAllNodes.getDofValue('DISPLACEMENT_X',tstep);
                 end
                 if find(ismember(obj.model.getAllNodes.getDofMap.keys, 'DISPLACEMENT_Y'))
-                    disp(:,2) = obj.model.getAllNodes.getDofValue('DISPLACEMENT_Y',time);
+                    disp(:,2) = obj.model.getAllNodes.getDofValue('DISPLACEMENT_Y',tstep);
                 end
                 if find(ismember(obj.model.getAllNodes.getDofMap.keys, 'DISPLACEMENT_Z'))
-                    disp(:,3) = obj.model.getAllNodes.getDofValue('DISPLACEMENT_Z',time);
+                    disp(:,3) = obj.model.getAllNodes.getDofValue('DISPLACEMENT_Z',tstep);
                 end
                 if find(ismember(obj.model.getAllNodes.getDofMap.keys, 'ROTATION_X'))
-                    rot(:,1) = obj.model.getAllNodes.getDofValue('ROTATION_X',time);
+                    rot(:,1) = obj.model.getAllNodes.getDofValue('ROTATION_X',tstep);
                 end
                 if find(ismember(obj.model.getAllNodes.getDofMap.keys, 'ROTATION_Y'))
-                    rot(:,2) = obj.model.getAllNodes.getDofValue('ROTATION_Y',time);
+                    rot(:,2) = obj.model.getAllNodes.getDofValue('ROTATION_Y',tstep);
                 end
                 if find(ismember(obj.model.getAllNodes.getDofMap.keys, 'ROTATION_Z'))
-                    rot(:,3) = obj.model.getAllNodes.getDofValue('ROTATION_Z',time);
+                    rot(:,3) = obj.model.getAllNodes.getDofValue('ROTATION_Z',tstep);
                 end
-                
-%                 fprintf(fid, [['Disp' num2str(0) num2str(i-1)] ' ' num2str(3) ' ' num2str(nPoints) ' float\n']);
-                
-                
+
                 fprintf(fid, 'Displacement %d %d float \n' , 3, nPoints);
                 for j = 1 : nPoints                    
                     fprintf(fid, '%f \t %f \t %f \n', disp(j,1), disp(j,2), disp(j,3));                    
                 end
-%                 fprintf(fid, [['Disp' num2str(0) num2str(i-1)] ' ' num2str(3) ' ' num2str(nPoints) ' float\n']);
-%                 fprintf(fid, 'Rotation %03d %d %d float \n', (i-1), 3, nPoints);
-%                 for j = 1 : nPoints                    
-%                     fprintf(fid, '%f \t %f \t %f \n', rot(j,1), rot(j,2), rot(j,3));                    
-%                 end
+
+                fprintf(fid, 'Rotation %d %d float \n', 3, nPoints);
+                for j = 1 : nPoints                    
+                    fprintf(fid, '%f \t %f \t %f \n', rot(j,1), rot(j,2), rot(j,3));                    
+                end
 
 %             end
  
