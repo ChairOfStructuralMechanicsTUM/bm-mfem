4fef50151416b8fc34720c0cb8b1723b707366c0
SebastianSiegel <SebastianSiegel@users.noreply.github.com>
2018-08-22 11:55:38 +0200

    vektor br weiter

--- core/substructure1.m
+++ core/substructure1.m
@@ -63,22 +63,26 @@ for i=1:hz
             br(j,i)={[nodematrix((j-1)*a+1,2:b).';nodematrix(j*a+1,2:b).';nodematrix((j-1)*a+2:j*a,b+1)]};
             K(j,i)={nodematrix((j-1)*a+1:j*a+1,(i-1)*b+1:i*b+1)};
         elseif i==1 && j==v %Fall 3 linke untere Ecke
-            bc(j,i)={[nodematrix(dim(1)-(a),1);nodematrix(dim(1)-(a),b+1);nodematrix(dim(1),b+1)]};
-            
+            bc(j,i)={[nodematrix(dim(1)-(a),1);nodematrix(dim(1)-a,b+1);nodematrix(dim(1),b+1)]};
+            br(j,i)={[nodematrix(dim(1)-a,2:b);nodematrix(dim(1)-a+1:dim(1)-1,b+1)]};
             K(j,i)={nodematrix((j-1)*a+1:dim(1),(i-1)*b+1:i*b+1)};  
         elseif j==1 && i~=1 && i~= hz    %Fall 4 erste Zeile Mitte
             if v==1 %falls nur eine Substruktur in vertikale Richtung vorhanden ist
                 bc(j,i)={[nodematrix(1,(i-1)*b+1);nodematrix(a,(i-1)*b+1);nodematrix(1,i*b+1);nodematrix(a,i*b+1)]};
+                br(j,i)={[nodematrix(2:j*a-1,(i-1)*b+1);nodematrix(2:j*a-1,i*b+1)]};
                 K(j,i)={nodematrix((j-1)*a+1:j*a,(i-1)*b+1:i*b+1)};
             else
                 bc(j,i)={[nodematrix((j-1)*a+1,(i-1)*b+1);nodematrix(j*a+1,(i-1)*b+1);nodematrix((j-1)*a+1,i*b+1);nodematrix(j*a+1,i*b+1)]};
+                br(j,i)={[nodematrix(2:j*a,(i-1)*b+1);nodematrix(a+1,(i-1)*b+2:i*b).';nodematrix(2:j*a,i*b+1)]};
                 K(j,i)={nodematrix((j-1)*a+1:j*a+1,(i-1)*b+1:i*b+1)};
             end
         elseif i>1 && i<hz && j>1 && j<v %Fall 5 Mitte Mitte
             bc(j,i)={[nodematrix((j-1)*a+1,(i-1)*b+1);nodematrix(j*a+1,(i-1)*b+1);nodematrix((j-1)*a+1,i*b+1);nodematrix(j*a+1,i*b+1)]};
+            br(j,i)={[nodematrix((j-1)*a+2:j*a,(i-1)*b+1);nodematrix((j-1)*a+1,(i-1)*b+2:i*b).';nodematrix(j*a+1,(i-1)*b+2:i*b).';nodematrix((j-1)*a+2:j*a,i*b+1)]};
             K(j,i)={nodematrix((j-1)*a+1:j*a+1,(i-1)*b+1:i*b+1)};
         elseif j==v && i~=1 && i~= hz %Fall 6 unterste Zeile Mitte
             bc(j,i)={[nodematrix((j-1)*a+1,(i-1)*b+1);nodematrix(dim(1),(i-1)*b+1);nodematrix((j-1)*a+1,i*b+1);nodematrix(dim(1),i*b+1)]};
+            
             K(j,i)={nodematrix((j-1)*a+1:dim(1),(i-1)*b+1:i*b+1)};
         elseif j==1 && i==hz %Fall 7 rechte obere Ecke
             if v==1 %falls nur eine Substruktur in vertikale Richtung vorhanden ist
