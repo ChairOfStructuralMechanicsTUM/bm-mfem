198a180a0cfb7b6012a38e4c1231975a7ebed3f3
SebastianSiegel <SebastianSiegel@users.noreply.github.com>
2018-08-22 17:53:34 +0200

    Umsortierung der Steifigkeitsmatrix

--- core/splitKmatrix.m
+++ core/splitKmatrix.m
@@ -1,4 +1,4 @@
-function [Kmatrix,Ksort]=splitKmatrix (n)
+function [Kmatrix,Ksort,Krr,Kcc,Krc,Kcr]=splitKmatrix (n)
 
 %Kmatrix= globale Systemsteifigkeitsmatrix, lokale Elemente bereits
 %assembliert
@@ -19,13 +19,24 @@ bc=[1;2];
 br=[3;4];
 %i
 i=[5;6;7;8];
+%r
+r=[i;br];
 %Vektor der Verschiebungsfreiheitsgrade
 u=[i;br;bc];
+%Umsortierte Stiefigkeitsmatrix
 Ksort=string(zeros(n,n));
-k=1;
 for i=1:n
     for j=1:n
         Ksort(i,j)=Kmatrix(u(i),u(j));
     end
 end
+%Steifigkeitsmatrix der remainder (br und i): Krr
+%Krr=string(zeros(size(r,1)));
+Krr=Ksort(1:size(r,1),1:size(r,1));
+%Steifigkeitsmatrix der corner Freiheitsgrade (bc):Kcc
+%Kcc=string(zeros(size(bc,1)));
+Kcc=Ksort(size(r,1)+1:size(r,1)+size(bc,1),size(r,1)+1:size(r,1)+size(bc,1));
+%Steifigkeitsmatrizen der Kombinierten Freiheitsgrade rbc, bcr: Krc, Kcr
+Krc=Ksort(1:size(r,1),size(r,1)+1:size(Ksort,1));
+Kcr=Ksort(size(r,1)+1:size(Ksort,1),1:size(r,1));
 end
