f51a0b12485768c6267c951865225ce1f6226ac9
franzi-mittermeier <franzi-mittermeier@users.noreply.github.com>
2018-08-23 10:58:45 +0200

    doing parameter studies

--- examples/bloch_gidinput_test.m
+++ examples/bloch_gidinput_test.m
@@ -1,14 +1,14 @@
 clear
 close all
 
-io=MdpaInput('Cell_Input.mdpa'); %specify input file
+io=MdpaInput('cell_circle.mdpa'); %specify input file
 model = io.readModel(); %read the model
 
 model.getAllNodes.addDof(["DISPLACEMENT_X","DISPLACEMENT_Y"]);
 
-innerNodes = model.getModelPart('GENERIC_Inner_Points').getNodes()
-id = getId(innerNodes) 
-coords = getCoords(innerNodes)
+innerNodes = model.getModelPart('GENERIC_Inner_circle').getNodes();
+id = getId(innerNodes); 
+coords = getCoords(innerNodes);
 % leftNodes2 = model.getModelPart('GENERIC_leftNodes2').getNodes()
 % rightNodes = model.getModelPart('GENERIC_rightNodes').getNodes()
 % rightNodes2 = model.getModelPart('GENERIC_rightNodes2').getNodes()
@@ -20,28 +20,28 @@ model.getAllElements.setPropertyValue('POISSON_RATIO',0.34);
 model.getAllElements.setPropertyValue('NUMBER_GAUSS_POINT',2);
 model.getAllElements.setPropertyValue('DENSITY',2699);
 
-model.addNewNode(244,0.1,0.075,0);
-model.addNewElement('SpringDamperElement3d2n',188,[59 244]);
-model.addNewElement('SpringDamperElement3d2n',189,[244 159]);
-model.addNewElement('ConcentratedMassElement3d1n',190, 244);
-
-model.getNode(59).addDof("DISPLACEMENT_Z");
-model.getNode(159).addDof("DISPLACEMENT_Z");
-model.getNode(244).addDof(["DISPLACEMENT_X","DISPLACEMENT_Y","DISPLACEMENT_Z"]);
-
-model.getNode(59).fixDof('DISPLACEMENT_Z');
-model.getNode(159).fixDof('DISPLACEMENT_Z');
-model.getNode(244).fixDof('DISPLACEMENT_Y');
-model.getNode(244).fixDof('DISPLACEMENT_Z');
-
-model.getElement(188).setPropertyValue('ELEMENTAL_STIFFNESS',7e5);
-model.getElement(188).setPropertyValue('ELEMENTAL_DAMPING',0);
-model.getElement(189).setPropertyValue('ELEMENTAL_STIFFNESS',7e5);
-model.getElement(189).setPropertyValue('ELEMENTAL_DAMPING',0);
-
-model.getElement(190).setPropertyValue('ELEMENTAL_MASS',7e2);
-model.getElement(190).setPropertyValue('VOLUME_ACCELERATION',10);
-
+% model.addNewNode(434,0.015,0.01,0);
+% model.addNewElement('SpringDamperElement3d2n',367,[254 434]);
+% model.addNewElement('SpringDamperElement3d2n',368,[434 115]);
+% model.addNewElement('ConcentratedMassElement3d1n',369, 434);
+% 
+% model.getNode(254).addDof("DISPLACEMENT_Z");
+% model.getNode(115).addDof("DISPLACEMENT_Z");
+% model.getNode(434).addDof(["DISPLACEMENT_X","DISPLACEMENT_Y","DISPLACEMENT_Z"]);
+% 
+% model.getNode(254).fixDof('DISPLACEMENT_Z');
+% model.getNode(115).fixDof('DISPLACEMENT_Z');
+% model.getNode(434).fixDof('DISPLACEMENT_Y');
+% model.getNode(434).fixDof('DISPLACEMENT_Z');
+% 
+% model.getElement(367).setPropertyValue('ELEMENTAL_STIFFNESS',7e5);
+% model.getElement(367).setPropertyValue('ELEMENTAL_DAMPING',0);
+% model.getElement(368).setPropertyValue('ELEMENTAL_STIFFNESS',7e5);
+% model.getElement(368).setPropertyValue('ELEMENTAL_DAMPING',0);
+% 
+% model.getElement(369).setPropertyValue('ELEMENTAL_MASS',7e2);
+% model.getElement(369).setPropertyValue('VOLUME_ACCELERATION',10);
+% 
 solver = BlochInverse1D(model);
 assembling = SimpleAssembler(model);
 
@@ -73,5 +73,6 @@ for j = 1:nob
     hold on
 end
 
-v=Visualization(model); %set up visualization
-v.plotUndeformed()  %visualize
+% fig=figure
+% v=Visualization(model); 
+% v.plotUndeformed()  
