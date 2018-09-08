3a31bed4b9ee46bc33424d3e3b9187597eb7f807
SebastianSiegel <SebastianSiegel@users.noreply.github.com>
2018-09-08 16:24:46 +0200

    tests

---
+++ core/splitKmatrixO1.m
@@ -0,0 +1,42 @@
+function [Ksort,Krr,Kcc,Krc,Kcr]=splitKmatrixO1 (Kmatrix,n)
+
+%Kmatrix= globale Systemsteifigkeitsmatrix, lokale Elemente bereits
+%assembliert
+%System: Fachwerk mit Grundelement 1; 4 Elemente 4 Knoten 8 globalen Dofs
+%enspricht linker oberer Ecke einer minimalen Substruktur
+%% setup 
+%Kmatrix
+% Kmatrix=string(ones(n));
+% for i=1:n
+%     for j=1:n
+%         id=[i,j];
+%         Kmatrix(i,j)=['K' num2str(id(1)) num2str(id(2))];
+%     end
+% end
+%bc: Freiheitsgrade der Eckknoten
+bc=[4;5;8;9];
+%br
+br=[6;7];
+%i
+i=[1;2;3];
+%r
+r=[i;br];
+%Vektor der Verschiebungsfreiheitsgrade
+u=[i;br;bc];
+%Umsortierte Stiefigkeitsmatrix
+Ksort=zeros(n,n);
+for i=1:n
+    for j=1:n
+        Ksort(i,j)=Kmatrix(u(i),u(j));
+    end
+end
+%Steifigkeitsmatrix der remainder (br und i): Krr
+%Krr=string(zeros(size(r,1)));
+Krr=Ksort(1:size(r,1),1:size(r,1));
+%Steifigkeitsmatrix der corner Freiheitsgrade (bc):Kcc
+%Kcc=string(zeros(size(bc,1)));
+Kcc=Ksort(size(r,1)+1:size(r,1)+size(bc,1),size(r,1)+1:size(r,1)+size(bc,1));
+%Steifigkeitsmatrizen der Kombinierten Freiheitsgrade rbc, bcr: Krc, Kcr
+Krc=Ksort(1:size(r,1),size(r,1)+1:size(Ksort,1));
+Kcr=Ksort(size(r,1)+1:size(Ksort,1),1:size(r,1));
+end
